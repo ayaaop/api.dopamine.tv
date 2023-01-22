b1476a4ca5f5a739738d56cb6bba0589d14a3127
dopamine-app <123301344+dopamine-app@users.noreply.github.com>
2023-01-22 23:14:05 +0600

    Update news.kt

--- server/news.kt
+++ server/news.kt
@@ -1,2 +1,8 @@
 [ {
-"ava":"
+"ava":"https://dopamine-app.github.io/api.dopamine.tv/server/img/channels4_profile.jpg",
+"user":"Dopamine Voice",
+"title":"Test",
+"message":"Test Messaging",
+"poster":"null",
+"data":"22 қаңтар - 23:13"
+} ]
