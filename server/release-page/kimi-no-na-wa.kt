ce4f8cb0b50e57950f49984b8d7b6af11a54bd34
dopamine-app <123301344+dopamine-app@users.noreply.github.com>
2023-01-24 09:15:58 +0600

    Update kimi-no-na-wa.kt

--- server/release-page/kimi-no-na-wa.kt
+++ server/release-page/kimi-no-na-wa.kt
@@ -11,5 +11,6 @@
 "info":"Токио жігіті Таки мен провинциялық қыз Мицуханың арасында біртүрлі байланыс бар екенін білінеді. Түстерінде олар бір-бірінің өмірлерін сүреді. Бірақ бір күні бұл қабілет қалай пайда болса,солай кенеттен жоғалып кетеді. Таки ештеңеге қарамастан Мицуханы табуға бел буады.",
 "unavailable":"null",
 "comments":"https://vk.com/widget_comments.php?app=8140038&amp;width=100%25&amp;_ver=1&amp;limit=15&amp;height=0&amp;mini=auto&amp;norealtime=0&amp;page=0&amp;status_publish=0&amp;attach=&amp;url=https%3A%2F%2Fdopaminevoice.kz%2Fkimi_no_nawa&amp;title=%D0%A1%D0%B5%D0%BD%D1%96%D2%A3%20%D0%95%D1%81%D1%96%D0%BC%D1%96%D2%A3%20%7C%20Dopamine%20Voice&amp;description=Dopamine%20voice%20-%20%D0%A1%D0%B5%D0%BD%D1%96%D2%A3%20%D0%95%D1%81%D1%96%D0%BC%D1%96%D2%A3&amp;image=&amp;startWidth=363&amp;referrer=&amp;185da415f37",
-"height":"2000"
+"height":"2000",
+"id":"kimi-no-na-wa"
 } ]
