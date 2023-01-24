183f75ef29e620c4b546c3cf43d6d6c719e37760
dopamine-app <123301344+dopamine-app@users.noreply.github.com>
2023-01-24 22:05:34 +0600

    Create chainsaw-man.kt

---
+++ server/e-page/episode/chainsaw-man.kt
@@ -0,0 +1,37 @@
+[ {
+"episode":"1 бөлім",
+"is":"https://ok.ru/videoembed/4187764230857"
+},
+{"episode":"2 бөлім",
+"is":"https://ok.ru/videoembed/4226012744393"
+},
+{"episode":"3 бөлім",
+"is":"https://ok.ru/videoembed/4256709216969"
+},
+{"episode":"4 бөлім",
+"is":"https://ok.ru/videoembed/4301457525449"
+},
+{"episode":"5 бөлім",
+"is":"https://ok.ru/videoembed/4332635228873"
+},
+{"episode":"6 бөлім",
+"is":"https://ok.ru/videoembed/4378909018825"
+},
+{"episode":"7 бөлім",
+"is":"https://ok.ru/videoembed/4410593512137"
+},
+{"episode":"8 бөлім",
+"is":"https://ok.ru/videoembed/4441444649673"
+},
+{"episode":"9 бөлім",
+"is":"https://ok.ru/videoembed/4463186086601"
+},
+{"episode":"10 бөлім",
+"is":"https://ok.ru/videoembed/4483693349577"
+},
+{"episode":"11 бөлім",
+"is":"https://ok.ru/videoembed/4535153330889"
+},
+{"episode":"12 бөлім",
+"is":"https://ok.ru/videoembed/4538467683017"
+} ]
