b212ffb6c74c184a5b2c1eed79184fcb5faa7bc0
dopamine-app <123301344+dopamine-app@users.noreply.github.com>
2023-01-24 15:46:46 +0600

    Update release.kt

--- test-server/release.kt
+++ test-server/release.kt
@@ -3,7 +3,7 @@
 "txt":"Hunter x Hunter"
 },
 {"poster":"https://m.media-amazon.com/images/I/71aoeOhdNnL._AC_UF894,1000_QL80_.jpg",
-"txt":"Hunter x Hunter 2",
+"txt":"Hunter x Hunter 2"
 },
 {"poster":"https://m.media-amazon.com/images/I/71aoeOhdNnL._AC_UF894,1000_QL80_.jpg",
 "txt":"Hunter x Hunter 3"
