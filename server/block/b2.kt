8b809c84a5d67a9bb70cb54cf5b56c9dbad9573c
dopamine-app <123301344+dopamine-app@users.noreply.github.com>
2023-01-24 21:41:56 +0600

    Update b2.kt

--- server/block/b2.kt
+++ server/block/b2.kt
@@ -1,6 +1,7 @@
 [ {
 "poster":"https://dopaminevoice.kz/img/howl_2.png",
-"episode":"1/1"
+"episode":"1/1",
+"id":"hauru-no-ugoku-shiro"
 },
 {"poster":"https://dopaminevoice.kz/img/kimi_poster.png",
 "episode":"1/1",
