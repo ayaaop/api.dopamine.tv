2e14d960dfeaf9389f8e0e4c9b8e86743a992d63
dopamine-app <123301344+dopamine-app@users.noreply.github.com>
2023-01-24 22:10:23 +0600

    Create chainsaw-man.kt

---
+++ server/release-page/chainsaw-man.kt
@@ -0,0 +1,16 @@
+[ {
+"poster":"https://dopaminevoice.kz/img/chainsaw_poster.png",
+"title":"Шынжырлы Ара Адам",
+"title_orignal":"Chensō Man",
+"country":"Япония, 2022 жыл",
+"i_video":"12 бөлім. ~25 минут",
+"status":"Көп бөлімді, шықты",
+"studio":"Студия MAPPA",
+"note":"null",
+"note_li":"null",
+"info":"Дэнжи тек қарапайым адамдар секілді өмір сүргісі келді, бірақ әкесінен қалған қарыздарын өтеу үшін ол заңсыз жын аулауға мәжбүр болып, бір күні қайтыс болды. Оның ерекше үй жануары Почита қандай да бір жолмен оның өлі денесімен біріктірілді, енді жігіт аяқ-қолдарын шынжырлы араға айналдыра алады. Мұндай құнды жаратылысты Қоғамдық қауіпсіздік бюросы қайтадан жындарды аулау үшін өз қолына алады - бұл жолы ресми түрде.",
+"unavailable":"null",
+"comments":"https://vk.com/widget_comments.php?app=8140038&amp;width=100%25&amp;_ver=1&amp;limit=15&amp;height=0&amp;mini=auto&amp;norealtime=0&amp;page=0&amp;status_publish=0&amp;attach=&amp;url=https%3A%2F%2Fdopaminevoice.kz%2Fchainsawman_1-1&amp;title=%D0%A8%D1%8B%D0%BD%D0%B6%D1%8B%D1%80%D0%BB%D1%8B%20%D0%90%D1%80%D0%B0%20%D0%90%D0%B4%D0%B0%D0%BC%201-%D0%9C%D0%B0%D1%83%D1%81%D1%8B%D0%BC%20%7C%20Dopamine%20Voice&amp;description=Dopamine%20Voice%20-%20%231%20%D2%9A%D0%B0%D0%B7%D0%B0%D2%9B%D1%88%D0%B0%20%D0%90%D0%BD%D0%B8%D0%BC%D0%B5%20%D1%81%D0%B0%D0%B9%D1%82%D1%8B.&amp;image=&amp;startWidth=363&amp;referrer=&amp;185e48852ea",
+"height":"5000",
+"id":"chainsaw-man"
+} ]
