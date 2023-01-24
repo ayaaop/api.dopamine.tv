355dca28494988a5b52c6b23806a24f4f43834eb
dopamine-app <123301344+dopamine-app@users.noreply.github.com>
2023-01-24 21:57:42 +0600

    Update b1.kt

--- server/block/b1.kt
+++ server/block/b1.kt
@@ -1,7 +1,9 @@
 [ {
 "poster":"https://dopaminevoice.kz/img/aot_poster_1.jpg",
-"episode":"10/25"
+"episode":"10/25",
+"id":"attack-on-titan"
 },
 {"poster":"https://dopaminevoice.kz/img/chainsaw_poster.png",
-"episode":"12/12"
+"episode":"12/12",
+"id":"chainsaw-man"
 } ]
