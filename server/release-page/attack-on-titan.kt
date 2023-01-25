2ba9b36c516a0036616d732ac1723eb9b22ca2a4
dopamine-app <123301344+dopamine-app@users.noreply.github.com>
2023-01-25 17:16:32 +0600

    Update attack-on-titan.kt

--- server/release-page/attack-on-titan.kt
+++ server/release-page/attack-on-titan.kt
@@ -11,6 +11,7 @@
 "unavailable":"null",
 "comments":"https://vk.com/widget_comments.php?app=8140038&amp;width=100%25&amp;_ver=1&amp;limit=15&amp;height=0&amp;mini=auto&amp;norealtime=0&amp;page=0&amp;status_publish=0&amp;attach=&amp;url=https%3A%2F%2Fdopaminevoice.kz%2Faot_1-1&amp;title=%D0%A2%D0%B8%D1%82%D0%B0%D0%BD%D0%B4%D0%B0%D1%80%20%D0%A8%D0%B0%D0%B1%D1%83%D1%8B%D0%BB%D1%8B%201-%D0%9C%D0%B0%D1%83%D1%81%D1%8B%D0%BC%20%7C%20Dopamine%20Voice&amp;description=Dopamine%20Voice%20-%20%231%20%D2%9A%D0%B0%D0%B7%D0%B0%D2%9B%D1%88%D0%B0%20%D0%90%D0%BD%D0%B8%D0%BC%D0%B5%20%D1%81%D0%B0%D0%B9%D1%82%D1%8B.&amp;image=&amp;startWidth=363&amp;referrer=&amp;185e47302c1",
 "height":"1500",
+"description":"фэнтези, драма, хоррор",
 "id":"attack-on-titan"
 } ]
 
