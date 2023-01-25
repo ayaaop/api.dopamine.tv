47d2d272bc78d306cb695d39212e637f355b8723
dopamine-app <123301344+dopamine-app@users.noreply.github.com>
2023-01-25 17:18:07 +0600

    Update hauru-no-ugoku-shiro.kt

--- server/release-page/hauru-no-ugoku-shiro.kt
+++ server/release-page/hauru-no-ugoku-shiro.kt
@@ -12,5 +12,6 @@
 "unavailable":"null",
 "comments":"https://vk.com/widget_comments.php?app=8140038&amp;width=100%25&amp;_ver=1&amp;limit=15&amp;height=0&amp;mini=auto&amp;norealtime=0&amp;page=0&amp;status_publish=0&amp;attach=&amp;url=https%3A%2F%2Fdopaminevoice.kz%2Fhowls_castle&amp;title=%D0%A5%D0%B0%D1%83%D0%BB%D0%B4%D1%8B%D2%A3%20%D0%96%D0%B0%D1%8F%D1%83%20%D2%9A%D0%B0%D0%BC%D0%B0%D0%BB%D1%8B%20%7C%20Dopamine%20Voice%20%7C%20%D2%9A%D0%B0%D0%B7%D0%B0%D2%9B%D1%88%D0%B0%20%D0%90%D0%BD%D0%B8%D0%BC%D0%B5&amp;description=Dopamine%20voice%20-%20%D0%A1%D0%B5%D0%BD%D1%96%D2%A3%20%D0%95%D1%81%D1%96%D0%BC%D1%96%D2%A3&amp;image=&amp;startWidth=363&amp;referrer=&amp;185e46d8d4c",
 "height":"2000",
+"description":"фантастика, ертегі",
 "id":"hauru-no-ugoku-shiro"
 } ]
