9bd5f5c7cc8bbeb388649b79b731d9ebf66ae9b7
dopamine-app <123301344+dopamine-app@users.noreply.github.com>
2023-01-27 18:11:47 +0600

    Update v1.kt

--- server/poster/v1.kt
+++ server/poster/v1.kt
@@ -2,6 +2,6 @@
 "poster":"https://dopaminevoice.kz/img/puss.png",
 "id":"cat-in-boots"
 },
-{"poster":"https://dopamine-app.github.io/api.dopamine.tv/server/img/Gekijouban Jujutsu Kaisen 0.png",
+{"poster":"https://dopamine-app.github.io/api.dopamine.tv/server/img/Gekijouban Jujutsu Kaisen 0 - poster.png",
 "id":"gekijouban-jujutsu-kaisen-0"
 } ]
