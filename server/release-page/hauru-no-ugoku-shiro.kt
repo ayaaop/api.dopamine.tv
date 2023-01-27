3fcd8222bcfe520ccf7333c0227b1d1c27aea131
dopamine-app <123301344+dopamine-app@users.noreply.github.com>
2023-01-27 16:33:35 +0600

    Update hauru-no-ugoku-shiro.kt

--- server/release-page/hauru-no-ugoku-shiro.kt
+++ server/release-page/hauru-no-ugoku-shiro.kt
@@ -3,7 +3,7 @@
 "title":"Хаулдың Жаяу Қамалы",
 "title_orignal":"Hauru no ugoku shiro",
 "age":"0+",
-"country":"Япония, 2004 жыл",
+"country":"Жапония, 2004 жыл",
 "i_video":"1 бөлім. ~116 минут",
 "status":"Толықметражды, шықты",
 "studio":"Студия Ghibli",
