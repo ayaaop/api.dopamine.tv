538e6d586a996d7695231e0a3a97b315faf5a221
dopamine-app <123301344+dopamine-app@users.noreply.github.com>
2023-01-28 10:04:00 +0600

    Update v1.kt

--- server/poster/v1.kt
+++ server/poster/v1.kt
@@ -4,4 +4,7 @@
 },
 {"poster":"https://dopamine-app.github.io/api.dopamine.tv/server/img/Gekijouban Jujutsu Kaisen 0 - poster.png",
 "id":"gekijouban-jujutsu-kaisen-0"
+},
+{"poster":"https://www.icegif.com/wp-content/uploads/2022/11/icegif-790.gif",
+"id":"chainsaw-man"
 } ]
