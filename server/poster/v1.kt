385a374a8cb2b6f35e49192f8efe30fd3630fe8f
dopamine-app <123301344+dopamine-app@users.noreply.github.com>
2023-02-12 20:16:13 +0600

    Update v1.kt

--- server/poster/v1.kt
+++ server/poster/v1.kt
@@ -1,10 +1,10 @@
 [ {
-"poster":"https://dopamine-app.github.io/api.dopamine.tv/server/img/poster9201.png",
+"poster":"https://dopamine-app.github.io/api.dopamine.tv/server/img/dop2.png",
 "id":"gekijouban-jujutsu-kaisen-0"
 },
 {"poster":"https://dopaminevoice.kz/img/puss.png",
 "id":"cat-in-boots"
 },
-{"poster":"https://www.icegif.com/wp-content/uploads/2022/11/icegif-790.gif",
+{"poster":"https://dopamine-app.github.io/api.dopamine.tv/server/img/dop1.png",
 "id":"chainsaw-man"
 } ]
