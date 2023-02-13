b67757532ce3c5fb5a2d66983b85316cbc95e1bb
dopamine-app <123301344+dopamine-app@users.noreply.github.com>
2023-02-13 22:07:07 +0600

    Update sponsor.kt

--- application/donuts/sponsor.kt
+++ application/donuts/sponsor.kt
@@ -2,8 +2,4 @@
 "avatars":"https://upload.wikimedia.org/wikipedia/commons/thumb/a/a6/Anonymous_emblem.svg/800px-Anonymous_emblem.svg.png",
 "nickname":"Anonymous",
 "tenge":"2500 теңге"
-},
-{"avatars":"https://i.gifer.com/cp.gif",
-"nickname":"Seele",
-"tenge":"2000 теңге"
 } ]
