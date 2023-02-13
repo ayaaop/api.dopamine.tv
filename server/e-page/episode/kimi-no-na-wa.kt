e9455971e8969163605c887dd55b415fdce1cfed
dopamine-app <123301344+dopamine-app@users.noreply.github.com>
2023-02-13 15:17:55 +0600

    Update kimi-no-na-wa.kt

--- server/e-page/episode/kimi-no-na-wa.kt
+++ server/e-page/episode/kimi-no-na-wa.kt
@@ -1,5 +1,5 @@
 [ {
 "episode":"Фильм",
-"is":"https://firebasestorage.googleapis.com/v0/b/dopamine-voice.appspot.com/o/Kimi%20no%20na%20wa%2F%D0%A1%D0%B5%D0%BD%D1%96%D2%A3%20%D0%B5%D1%81%D1%96%D0%BC%D1%96%D2%A3.mp4?alt=media&token=fae96c07-d481-401f-a794-568ca1f158ec",
-"url":"https://firebasestorage.googleapis.com/v0/b/dopamine-voice.appspot.com/o/Kimi%20no%20na%20wa%2F%D0%A1%D0%B5%D0%BD%D1%96%D2%A3%20%D0%B5%D1%81%D1%96%D0%BC%D1%96%D2%A3.mp4?alt=media&token=fae96c07-d481-401f-a794-568ca1f158ec"
+"is":"https://video.sibnet.ru/shell.php?videoid=4754278&share=1",
+"url":""
 } ]
