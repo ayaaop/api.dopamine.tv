e3308381b79df5a47177f518cd0903f7b3559c39
dopamine-app <123301344+dopamine-app@users.noreply.github.com>
2023-02-15 22:25:31 +0600

    Update gekijouban-jujutsu-kaisen-0.kt

--- server/release-page/gekijouban-jujutsu-kaisen-0.kt
+++ server/release-page/gekijouban-jujutsu-kaisen-0.kt
@@ -1,6 +1,6 @@
 [ {
 "poster":"https://dopamine-app.github.io/api.dopamine.tv/server/img/Gekijouban Jujutsu Kaisen 0.png",
-"title":"Сыйқырлы шайқас 0",
+"title":"Сиқырлы шайқас 0",
 "title_orignal":"Gekijouban Jujutsu Kaisen 0",
 "age":"18+",
 "country":"Жапония, 2021 жыл",
