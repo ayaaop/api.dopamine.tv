74bee950e109c04149bc1928ecf023a50556ed37
dopamine-app <123301344+dopamine-app@users.noreply.github.com>
2023-02-16 10:50:08 +0600

    Update hauru-no-ugoku-shiro.kt

--- server/e-page/episode/hauru-no-ugoku-shiro.kt
+++ server/e-page/episode/hauru-no-ugoku-shiro.kt
@@ -1,5 +1,5 @@
 [ {
 "episode":"Фильм",
-"is":"https://firebasestorage.googleapis.com/v0/b/dopamine-voice.appspot.com/o/Hauru%20no%20ugoku%20shiro%2F%D0%A5%D0%B0%D1%83%D0%BB%D0%B4%D1%8B%D2%A3%20%D0%96%D0%B0%D1%8F%D1%83%20%D2%9A%D0%B0%D0%BC%D0%B0%D0%BB%D1%8B%20%D0%A4%D0%98%D0%9D%D0%90%D0%9B.mp4?alt=media&token=9d54fdfe-6516-424d-b5d5-74f5aa18029e",
-"url":"https://firebasestorage.googleapis.com/v0/b/dopamine-voice.appspot.com/o/Hauru%20no%20ugoku%20shiro%2F%D0%A5%D0%B0%D1%83%D0%BB%D0%B4%D1%8B%D2%A3%20%D0%96%D0%B0%D1%8F%D1%83%20%D2%9A%D0%B0%D0%BC%D0%B0%D0%BB%D1%8B%20%D0%A4%D0%98%D0%9D%D0%90%D0%9B.mp4?alt=media&token=9d54fdfe-6516-424d-b5d5-74f5aa18029e"
+"is":"https://ok.ru/videoembed/3862613789385",
+"url":"https://ok.ru/videoembed/3862613789385"
 } ]
