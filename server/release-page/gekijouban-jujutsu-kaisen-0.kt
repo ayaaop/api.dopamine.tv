55f6fc3b53eba383421f6d73ef5c8050498276af
dopamine-app <123301344+dopamine-app@users.noreply.github.com>
2023-02-16 13:37:24 +0600

    Update gekijouban-jujutsu-kaisen-0.kt

--- server/release-page/gekijouban-jujutsu-kaisen-0.kt
+++ server/release-page/gekijouban-jujutsu-kaisen-0.kt
@@ -11,10 +11,9 @@
 "note_li":"null",
 "info":"Юта Оккоцуды апаттан қайтыс болған балалық досының елесі аңдыйды. Дегенмен, ол өлер алдындағы момын қыз емес, жігіт басқара алмайтын құбыжық иесі. Сондықтан, сиқыршылар Ютаны ұстап алғанда, бала енді ешкім қарғыстан зардап шекпейтініне қуанады. Оны ұстаған сиқыр шебері Сатору Годжо жас жігітті Риканы басқаруды және адамдарға көмектесуді үйрену үшін сиқыр мектебіне түсуге көндіреді. Алайда Юта көп ұзамай зұлым рухтар әлемдегі ең қауіпті дүние емес екенін біледі.",
 "announcement":"null",
-"comments":"null",
-"height":"5000",
+"comments":"https://vk.com/widget_comments.php?app=8140038&amp;width=100%25&amp;_ver=1&amp;limit=15&amp;height=0&amp;mini=auto&amp;norealtime=0&amp;page=0&amp;status_publish=0&amp;attach=&amp;url=https%3A%2F%2Fdopaminevoice.kz%2Fjujutsukaisen0&amp;title=%D0%A1%D0%B8%D2%9B%D1%8B%D1%80%D0%BB%D1%8B%20%D0%A8%D0%B0%D0%B9%D2%9B%D0%B0%D1%81%200%20%7C%20Dopamine%20Voice%20%7C%20%D2%9A%D0%B0%D0%B7%D0%B0%D2%9B%D1%88%D0%B0%20%D0%90%D0%BD%D0%B8%D0%BC%D0%B5&amp;description=Dopamine%20voice%20-%20%D0%A1%D0%B5%D0%BD%D1%96%D2%A3%20%D0%95%D1%81%D1%96%D0%BC%D1%96%D2%A3&amp;image=&amp;startWidth=363&amp;referrer=&amp;1865926cffa",
+"height":"150",
 "description":"экшен, тылсым күштер",
-"com":"null",
 "unavailable":"null",
 "data":"15-ші ақпанда",
 "id":"gekijouban-jujutsu-kaisen-0"
