2ff51c6285fe520a5c141fd19db3f4dcd6ea76a0
dopamine-app <123301344+dopamine-app@users.noreply.github.com>
2023-02-16 19:53:21 +0600

    Update gekijouban-jujutsu-kaisen-0.kt

--- server/e-page/episode/gekijouban-jujutsu-kaisen-0.kt
+++ server/e-page/episode/gekijouban-jujutsu-kaisen-0.kt
@@ -1,5 +1,5 @@
 [ {
 "episode":"Фильм",
 "is":"https://ok.ru/videoembed/4686808746697",
-"url":"https://ok.ru/videoembed/4686808746697"
+"url":"https://firebasestorage.googleapis.com/v0/b/dopamine-voice.appspot.com/o/Jujutsu%20Kaisen%200%2F%D0%A4%D0%B8%D0%BD%D0%B0%D0%BB.mp4?alt=media&token=f8f69812-6979-4094-8d19-fae22d25ea62"
 } ]
