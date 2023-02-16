109bd0b8e1ecaefd4f884eba8b71f4d5fac75d7f
dopamine-app <123301344+dopamine-app@users.noreply.github.com>
2023-02-16 10:58:29 +0600

    Update chainsaw-man.kt

--- server/e-page/episode/chainsaw-man.kt
+++ server/e-page/episode/chainsaw-man.kt
@@ -20,30 +20,30 @@
 "url":"https://firebasestorage.googleapis.com/v0/b/dopamine-voice.appspot.com/o/Chainsaw%20Man%2FChainsaw%20Man%20EP%205.mp4?alt=media&token=07ff71c8-580b-460c-a1f6-7ca128bd8c3d"
 },
 {"episode":"6 бөлім",
-"is":"",
+"is":"https://ok.ru/videoembed/4378909018825",
 "url":"https://firebasestorage.googleapis.com/v0/b/dopamine-voice.appspot.com/o/Chainsaw%20Man%2FChainsaw%20Man%206%20EP.mp4?alt=media&token=b4c2a6c4-adbe-479b-8455-08ac51683f83"
 },
 {"episode":"7 бөлім",
-"is":"",
+"is":"https://ok.ru/videoembed/4410593512137",
 "url":"https://firebasestorage.googleapis.com/v0/b/dopamine-voice.appspot.com/o/Chainsaw%20Man%2FChainsaw%20Man%207.mp4?alt=media&token=722a8101-c88e-44b3-9a32-1a4bdb81ac2d"
 },
 {"episode":"8 бөлім",
-"is":"",
+"is":"https://ok.ru/videoembed/4441444649673",
 "url":"https://firebasestorage.googleapis.com/v0/b/dopamine-voice.appspot.com/o/Chainsaw%20Man%2FChainsaw%20Man%20EP%208.mp4?alt=media&token=c27ff38f-e637-4579-ac42-56737329d155"
 },
 {"episode":"9 бөлім",
-"is":"",
+"is":"https://ok.ru/videoembed/4463186086601",
 "url":"https://firebasestorage.googleapis.com/v0/b/dopamine-voice.appspot.com/o/Chainsaw%20Man%2FChainsaw%20Man%20EP%209.mp4?alt=media&token=f5df828f-50b9-435a-bce8-ab27f710a6ae"
 },
 {"episode":"10 бөлім",
-"is":"",
+"is":"https://ok.ru/videoembed/4483693349577",
 "url":"https://firebasestorage.googleapis.com/v0/b/dopamine-voice.appspot.com/o/Chainsaw%20Man%2FChainsaw%20Man%2010.mp4?alt=media&token=9c228e3f-2ecd-4077-84a2-88f9797f69af"
 },
 {"episode":"11 бөлім",
-"is":"",
+"is":"https://ok.ru/videoembed/4535153330889",
 "url":"https://firebasestorage.googleapis.com/v0/b/dopamine-voice.appspot.com/o/Chainsaw%20Man%2FChainsaw%20Man%2011.mp4?alt=media&token=d91307a9-1f61-403d-a2e8-e29e02e7d06f"
 },
 {"episode":"12 бөлім",
-"is":"",
+"is":"https://ok.ru/videoembed/4538467683017",
 "url":"https://firebasestorage.googleapis.com/v0/b/dopamine-voice.appspot.com/o/Chainsaw%20Man%2FChainsaw%20Man%2012.mp4?alt=media&token=aba85a7f-c6ea-47e7-b0a8-68f5933d270c"
 } ]
