1556e0eea0d9c39d03fe600c2029010b634ea896
dopamine-app <123301344+dopamine-app@users.noreply.github.com>
2023-02-18 17:41:38 +0600

    Update information.kt

--- application/mini/information.kt
+++ application/mini/information.kt
@@ -1,6 +1,7 @@
 [ {
+"li":"null",
 "txt":"Сіздерді телеграм чатымызға шақырамыз »",
-"url":"https://t.me/dopamine_apps",
+"url":"https://t.me/dopamine_chat",
 "color":"#ff0088cc",
 "radius":"35"
 } ]
