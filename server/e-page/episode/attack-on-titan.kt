a8dfa32bc35315674586064c040dc5cfc26d7c0f
dopamine-app <123301344+dopamine-app@users.noreply.github.com>
2023-02-18 16:56:08 +0600

    Update attack-on-titan.kt

--- server/e-page/episode/attack-on-titan.kt
+++ server/e-page/episode/attack-on-titan.kt
@@ -1,31 +1,41 @@
 [ {
 "episode":"1 бөлім",
-"is":"https://ok.ru/videoembed/3488451332809"
+"is":"https://ok.ru/videoembed/3488451332809",
+"url":"null"
 },
 {"episode":"2 бөлім",
-"is":"https://ok.ru/videoembed/3488560712393"
+"is":"https://ok.ru/videoembed/3488560712393",
+"url":"null"
 },
 {"episode":"3 бөлім",
-"is":"https://ok.ru/videoembed/3488532269769"
+"is":"https://ok.ru/videoembed/3488532269769",
+"url":"null"
 },
 {"episode":"4 бөлім",
-"is":"https://ok.ru/videoembed/3488382520009"
+"is":"https://ok.ru/videoembed/3488382520009",
+"url":"null"
 },
 {"episode":"5 бөлім",
-"is":"https://ok.ru/videoembed/3504073214665"
+"is":"https://ok.ru/videoembed/3504073214665",
+"url":"null"
 },
 {"episode":"6 бөлім",
-"is":"https://video.sibnet.ru/shell.php?videoid=4676892"
+"is":"https://video.sibnet.ru/shell.php?videoid=4676892",
+"url":"null"
 },
 {"episode":"7 бөлім",
-"is":"https://video.sibnet.ru/shell.php?videoid=4676846"
+"is":"https://video.sibnet.ru/shell.php?videoid=4676846",
+"url":"null"
 },
 {"episode":"8 бөлім",
-"is":"https://ok.ru/videoembed/3648973507273"
+"is":"https://ok.ru/videoembed/3648973507273",
+"url":"null"
 },
 {"episode":"9 бөлім",
-"is":"https://video.sibnet.ru/shell.php?videoid=4676843"
+"is":"https://video.sibnet.ru/shell.php?videoid=4676843",
+"url":"null"
 },
 {"episode":"10 бөлім",
-"is":"https://video.sibnet.ru/shell.php?videoid=4683974"
+"is":"https://video.sibnet.ru/shell.php?videoid=4683974",
+"url":"null"
 } ]
