aa361903fce2f89e8e8213ba94d07e0e070238b4
dopamine-app <123301344+dopamine-app@users.noreply.github.com>
2023-02-25 19:42:29 +0600

    Create darling-in-franxxx.kt

---
+++ server/e-page/episode/darling-in-franxxx.kt
@@ -0,0 +1,101 @@
+[ {
+"episode":"1 бөлім",
+"is":"null",
+"url":"https://cache.libria.fun/videos/media/ts/5692/1/720/8018fcd96c0ae3efadd9f25385a6030e.m3u8"
+},
+{"episode":"2 бөлім",
+"is":"null",
+"url":"https://cache.libria.fun/videos/media/ts/5692/2/720/99b56df45da7a918d35dcb6234faa476.m3u8"
+},
+{"episode":"3 бөлім",
+"is":"null",
+"url":"https://cache.libria.fun/videos/media/ts/5692/3/720/eb05bd544e6deba09a2c4fa1715aa24f.m3u8"
+},
+{"episode":"4 бөлім",
+"is":"null",
+"url":"https://cache.libria.fun/videos/media/ts/5692/4/720/ad6833cd5c92398a6e382449d940912f.m3u8"
+},
+{"episode":"5 бөлім",
+"is":"null",
+"url":"https://cache.libria.fun/videos/media/ts/5692/5/720/79df3025533806f069f6f1412ccb09e9.m3u8"
+},
+{"episode":"6 бөлім",
+"is":"null",
+"url":"https://cache.libria.fun/videos/media/ts/5692/6/720/eb22cecc1c28d8b23097a91f71b54f74.m3u8"
+},
+{"episode":"7 бөлім",
+"is":"null",
+"url":"https://cache.libria.fun/videos/media/ts/5692/7/720/097e15366c853ef33635433342adbef1.m3u8"
+},
+{"episode":"8 бөлім",
+"is":"null",
+"url":"https://cache.libria.fun/videos/media/ts/5692/8/720/16b171d936d2024ee34793e555b84519.m3u8"
+},
+{"episode":"9 бөлім",
+"is":"null",
+"url":"https://cache.libria.fun/videos/media/ts/5692/9/720/6c0b6ccf9f622fff4934f059f4ac9cca.m3u8"
+},
+{"episode":"10 бөлім",
+"is":"null",
+"url":"https://cache.libria.fun/videos/media/ts/5692/10/720/0a3787842d4ecbb5913949989b4c6e75.m3u8"
+},
+{"episode":"11 бөлім",
+"is":"null",
+"url":""
+},
+{"episode":"12 бөлім",
+"is":"null",
+"url":""
+},
+{"episode":"13 бөлім",
+"is":"null",
+"url":""
+},
+{"episode":"14 бөлім",
+"is":"null",
+"url":""
+},
+{"episode":"15 бөлім",
+"is":"null",
+"url":""
+},
+{"episode":"16 бөлім",
+"is":"null",
+"url":""
+},
+{"episode":"17 бөлім",
+"is":"null",
+"url":""
+},
+{"episode":"18 бөлім",
+"is":"null",
+"url":""
+},
+{"episode":"19 бөлім",
+"is":"null",
+"url":""
+},
+{"episode":"20 бөлім",
+"is":"null",
+"url":""
+},
+{"episode":"21 бөлім",
+"is":"null",
+"url":""
+},
+{"episode":"22 бөлім",
+"is":"null",
+"url":""
+},
+{"episode":"23 бөлім",
+"is":"null",
+"url":""
+},
+{"episode":"24 бөлім",
+"is":"null",
+"url":""
+},
+{"episode":"25 бөлім",
+"is":"null",
+"url":""
+} ]
