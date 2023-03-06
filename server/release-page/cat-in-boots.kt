fac4387e9f3760b1f6aad3416ccf00b95e490c41
dopamine-app <123301344+dopamine-app@users.noreply.github.com>
2023-03-06 13:38:57 +0600

    Update cat-in-boots.kt

--- server/release-page/cat-in-boots.kt
+++ server/release-page/cat-in-boots.kt
@@ -10,6 +10,7 @@
 "note":"Фильм әзірше серверде қолжетімсіз!\n\nФильмді біздің телеграм арнадан көре аласыздар: https://t.me/dopaminevoice_full/18",
 "info":"Тоғыз өмірінің сегізін құртып, артынан келген аңшы қасқырдан қорыққан Етік киген Мысық дәрігердің кеңесіне құлақ асып, жергілікті мысық ханымның үйіне көшіп барады. Онда ол қалыптасқан тәртіпке бағынуына тура келеді. Сақал өсіріп, ерлік пен шытырман оқиғаларға деген құштарлығын жоғалтады, бірақ ол бір жерде құлаған Соңғы Тілек жұлдызына жол көрсететін сиқырлы карта туралы білгенде қайтадан жорыққа шығады. Етік киген Мысық өз өмірін қайтару үшін Жұлдызды табуға бел буады.\nМысық болып киінген итпен бірге ол әйгілі сиқырлы заттарды жинаушы Джек Хорнердің картасын ұрлауға барады.",
 "unavailable":"null",
+"note_li":"null",
 "announcement":"null",
 "premium":"null",
 "comments":"https://vk.com/widget_comments.php?app=8140038&amp;width=100%25&amp;_ver=1&amp;limit=15&amp;height=0&amp;mini=auto&amp;norealtime=0&amp;page=0&amp;status_publish=0&amp;attach=&amp;url=https%3A%2F%2Fdopaminevoice.kz%2Fpussinbootsthelastwish&amp;title=%D0%95%D1%82%D1%96%D0%BA%20%D0%BA%D0%B8%D0%B3%D0%B5%D0%BD%20%D0%9C%D1%8B%D1%81%D1%8B%D2%9B%20%3A%20%D0%A1%D0%BE%D2%A3%D2%93%D1%8B%20%D0%A2%D1%96%D0%BB%D0%B5%D0%BA%20%7C%20Dopamine%20Voice%20%7C%20%D2%9A%D0%B0%D0%B7%D0%B0%D2%9B%D1%88%D0%B0%20%D0%90%D0%BD%D0%B8%D0%BC%D0%B5&amp;description=Dopamine%20voice%20-%20%D0%A1%D0%B5%D0%BD%D1%96%D2%A3%20%D0%95%D1%81%D1%96%D0%BC%D1%96%D2%A3&amp;image=&amp;startWidth=363&amp;referrer=&amp;186b2c71f79",
