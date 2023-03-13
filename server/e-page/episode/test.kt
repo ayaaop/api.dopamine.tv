e5ed1b2b1736d292656cab53064c722e5a23448e
dopamine-app <123301344+dopamine-app@users.noreply.github.com>
2023-03-13 21:06:57 +0600

    Update test.kt

--- server/e-page/episode/test.kt
+++ server/e-page/episode/test.kt
@@ -1,9 +1,53 @@
 [ {
 "episode":"1 бөлім",
-"is":"https://dopaminevoice.kz/video/chainsawman.mp4",
-"url":"https://dopaminevoice.kz/video/chainsawman.mp4"
+"is":"https://cache.libria.fun/videos/media/ts/8679/1/480/291a212eecf2060997e5bfa8bc1a0e63.m3u8",
+"url":"https://cache.libria.fun/videos/media/ts/8679/1/480/291a212eecf2060997e5bfa8bc1a0e63.m3u8"
 },
 {"episode":"2 бөлім",
-"is":"https://video.sibnet.ru/shell.php?videoid=3213858",
-"url":"https://video.sibnet.ru/shell.php?videoid=3213858"
+"is":"https://cache.libria.fun/videos/media/ts/8679/2/480/3f7aa747ec1ea5f700563d302500e456.m3u8",
+"url":"https://cache.libria.fun/videos/media/ts/8679/2/480/3f7aa747ec1ea5f700563d302500e456.m3u8"
+},
+{"episode":"3 бөлім",
+"is":"https://cache.libria.fun/videos/media/ts/8679/3/480/a114a54153cc44dc04610203bb479a2f.m3u8",
+"url":"https://cache.libria.fun/videos/media/ts/8679/3/480/a114a54153cc44dc04610203bb479a2f.m3u8"
+},
+{"episode":"4 бөлім",
+"is":"https://cache.libria.fun/videos/media/ts/8679/4/480/4913b69fa03cd9a1ff42460d64cc5cba.m3u8",
+"url":"https://cache.libria.fun/videos/media/ts/8679/4/480/4913b69fa03cd9a1ff42460d64cc5cba.m3u8"
+},
+{"episode":"5 бөлім",
+"is":"https://cache.libria.fun/videos/media/ts/8679/5/480/1a65f2a9fb87e694a5598a87df05b2fd.m3u8",
+"url":"https://cache.libria.fun/videos/media/ts/8679/5/480/1a65f2a9fb87e694a5598a87df05b2fd.m3u8"
+},
+{"episode":"6 бөлім",
+"is":"https://cache.libria.fun/videos/media/ts/8679/6/480/1057f05d6557e49afa11cbdc186034d0.m3u8",
+"url":"https://cache.libria.fun/videos/media/ts/8679/6/480/1057f05d6557e49afa11cbdc186034d0.m3u8"
+},
+{"episode":"7 бөлім",
+"is":"https://cache.libria.fun/videos/media/ts/8679/7/480/f0028acee671daf1e70cf6c5e1336373.m3u8",
+"url":"https://cache.libria.fun/videos/media/ts/8679/7/480/f0028acee671daf1e70cf6c5e1336373.m3u8"
+},
+{"episode":"8 бөлім",
+"is":"https://cache.libria.fun/videos/media/ts/8679/8/480/9933bd1c710caf9edbf41f7379b97a9c.m3u8",
+"url":"https://cache.libria.fun/videos/media/ts/8679/8/480/9933bd1c710caf9edbf41f7379b97a9c.m3u8"
+},
+{"episode":"9 бөлім",
+"is":"https://cache.libria.fun/videos/media/ts/8679/9/480/7fab80007f307b0dcd198726d15d7f64.m3u8",
+"url":"https://cache.libria.fun/videos/media/ts/8679/9/480/7fab80007f307b0dcd198726d15d7f64.m3u8"
+},
+{"episode":"10 бөлім",
+"is":"https://cache.libria.fun/videos/media/ts/8679/10/480/6a82fff472a819cdf7098fa9cb28bfcc.m3u8",
+"url":"https://cache.libria.fun/videos/media/ts/8679/10/480/6a82fff472a819cdf7098fa9cb28bfcc.m3u8"
+},
+{"episode":"11 бөлім",
+"is":"https://cache.libria.fun/videos/media/ts/8679/11/480/5affb496ac1a9f174bfeb56e715faab5.m3u8",
+"url":"https://cache.libria.fun/videos/media/ts/8679/11/480/5affb496ac1a9f174bfeb56e715faab5.m3u8"
+},
+{"episode":"12 бөлім",
+"is":"",
+"url":""
+},
+{"episode":"13 бөлім",
+"is":"",
+"url":""
 } ]
