31997c82aa4e74c69075f57daf47c0ec3d9b9d61
dopamine-app <123301344+dopamine-app@users.noreply.github.com>
2023-03-13 20:39:52 +0600

    Update test.kt

--- server/e-page/episode/test.kt
+++ server/e-page/episode/test.kt
@@ -2,4 +2,8 @@
 "episode":"1 бөлім",
 "is":"https://dopaminevoice.kz/video/chainsawman.mp4",
 "url":"https://dopaminevoice.kz/video/chainsawman.mp4"
+},
+{"episode":"2 бөлім",
+"is":"https://video.sibnet.ru/shell.php?videoid=3213858",
+"url":"https://video.sibnet.ru/shell.php?videoid=3213858"
 } ]
