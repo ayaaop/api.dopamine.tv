7744493f1d50dddb55891e1c20885ee04bfa8a64
dopamine-app <123301344+dopamine-app@users.noreply.github.com>
2023-03-13 15:53:21 +0600

    Update announcement.kt

--- server/announcement.kt
+++ server/announcement.kt
@@ -1,5 +1,11 @@
 [ {
-"poster":"https://m.media-amazon.com/images/M/MV5BNjRiNmNjMmMtN2U2Yi00ODgxLTk3OTMtMmI1MTI1NjYyZTEzXkEyXkFqcGdeQXVyNjAwNDUxODI@._V1_FMjpg_UX1000_.jpg",
+"poster":"https://dopamine-app.github.io/api.dopamine.tv/server/img/sen-to-chihiro-no-kamikakushi.png",
+"title":"Елес Ерткен (2001)",
+"data":"2023",
+"info":"Сипаттама берілген жоқ!",
+"id":"sen-to-chihiro-no-kamikakushi"
+},
+{"poster":"https://m.media-amazon.com/images/M/MV5BNjRiNmNjMmMtN2U2Yi00ODgxLTk3OTMtMmI1MTI1NjYyZTEzXkEyXkFqcGdeQXVyNjAwNDUxODI@._V1_FMjpg_UX1000_.jpg",
 "title":"Ажал дәптері",
 "data":"2023",
 "info":"Орта мектеп оқушысы Лайт Ягамиге Ажал қызметшісі, яғни Шинигами, Рюктің дәптері қолына түседі. Осы дәптерге аты жазылған әрбір адам өледі, сондықтан Лайт Ягами жер бетіндегі зұлымдықпен күресуді шешеді.",
