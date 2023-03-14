e1f42aa94e2c7c5708a5dd3998f87be882190f57
dopamine-app <123301344+dopamine-app@users.noreply.github.com>
2023-03-14 06:21:44 +0600

    Update announcement.kt

--- server/announcement.kt
+++ server/announcement.kt
@@ -1,8 +1,8 @@
 [ {
 "poster":"https://dopamine-app.github.io/api.dopamine.tv/server/img/sen-to-chihiro-no-kamikakushi.png",
-"title":"Елес Ерткен (2001)",
+"title":"Елес Ерткен",
 "data":"2023",
-"info":"Сипаттама берілген жоқ!",
+"info":"Чихиро анасы және әкесімен жаңа үйге көшіп барады.  Жолда адасып, олар таңғажайып дастархан күтіп тұрған бейтаныс шөлді қалаға тап болады. Чихироның ата-анасы ашкөздіктен тамақ жей бастаған кезде шошқаға айналып, зұлым сиқыршы Юбабаның тұтқынына айналады.  Енді сиқырлы жаратылыстар мен жұмбақ көріністердің арасында жалғыз қалған Чихиро ата-анасын қу кемпірдің сиқырынан қалай құтқару керектігін анықтауы керек.",
 "id":"sen-to-chihiro-no-kamikakushi"
 },
 {"poster":"https://m.media-amazon.com/images/M/MV5BNjRiNmNjMmMtN2U2Yi00ODgxLTk3OTMtMmI1MTI1NjYyZTEzXkEyXkFqcGdeQXVyNjAwNDUxODI@._V1_FMjpg_UX1000_.jpg",
