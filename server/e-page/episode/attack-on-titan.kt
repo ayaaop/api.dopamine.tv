1938102918cc1f82d3ac306d72037f7a1150b528
dopamine-app <123301344+dopamine-app@users.noreply.github.com>
2023-03-21 14:30:56 +0600

    Update attack-on-titan.kt

--- server/e-page/episode/attack-on-titan.kt
+++ server/e-page/episode/attack-on-titan.kt
@@ -1,41 +1,51 @@
 [ {
+"title":"Титандар Шабуылы",
 "episode":"1 бөлім",
 "is":"https://ok.ru/videoembed/3488451332809",
 "url":"null"
 },
-{"episode":"2 бөлім",
+{"title":"Титандар Шабуылы",
+"episode":"2 бөлім",
 "is":"https://ok.ru/videoembed/3488560712393",
 "url":"null"
 },
-{"episode":"3 бөлім",
+{"title":"Титандар Шабуылы",
+"episode":"3 бөлім",
 "is":"https://ok.ru/videoembed/3488532269769",
 "url":"null"
 },
-{"episode":"4 бөлім",
+{"title":"Титандар Шабуылы",
+"episode":"4 бөлім",
 "is":"https://ok.ru/videoembed/3488382520009",
 "url":"null"
 },
-{"episode":"5 бөлім",
+{"title":"Титандар Шабуылы",
+"episode":"5 бөлім",
 "is":"https://ok.ru/videoembed/3504073214665",
 "url":"null"
 },
-{"episode":"6 бөлім",
+{"title":"Титандар Шабуылы",
+"episode":"6 бөлім",
 "is":"https://video.sibnet.ru/shell.php?videoid=4676892",
 "url":"null"
 },
-{"episode":"7 бөлім",
+{"title":"Титандар Шабуылы",
+"episode":"7 бөлім",
 "is":"https://video.sibnet.ru/shell.php?videoid=4676846",
 "url":"null"
 },
-{"episode":"8 бөлім",
+{"title":"Титандар Шабуылы",
+"episode":"8 бөлім",
 "is":"https://ok.ru/videoembed/3648973507273",
 "url":"null"
 },
-{"episode":"9 бөлім",
+{"title":"Титандар Шабуылы",
+"episode":"9 бөлім",
 "is":"https://video.sibnet.ru/shell.php?videoid=4676843",
 "url":"null"
 },
-{"episode":"10 бөлім",
+{"title":"Титандар Шабуылы",
+"episode":"10 бөлім",
 "is":"https://video.sibnet.ru/shell.php?videoid=4683974",
 "url":"null"
 } ]
