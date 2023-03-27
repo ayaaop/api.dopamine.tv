4372f71ea09430da0d97666371971bcf3e15f92c
dopamine-app <123301344+dopamine-app@users.noreply.github.com>
2023-03-27 19:27:18 +0600

    Update cat-in-boots.kt

--- server/e-page/episode/cat-in-boots.kt
+++ server/e-page/episode/cat-in-boots.kt
@@ -1,5 +1,5 @@
 [ {
 "episode":"Фильм",
-"is":"https://alice.embedgram.com/file/8f399c4f32ef9f626dc8847291024f42/e2c6570a8d13c50a243cec39a3b25837a06f759e0a9d017f1266d5b5d3483c59e39f85a51d81e9ae97ba8abea14adae5797833fa2706f6ae9e4e0df614d8b5e3/1679587138/8pSjdAtJRPwpjL8wkJrUN+jcUkJNPMuOA7QW+XUxAyM=",
+"is":"https://alice.embedgram.com/file/13e6fbf154ede38661608e4206a16df8/e2c6570a8d13c50a243cec39a3b25837a06f759e0a9d017f1266d5b5d3483c59e39f85a51d81e9ae97ba8abea14adae5797833fa2706f6ae9e4e0df614d8b5e3/1679934344/8pSjdAtJRPwpjL8wkJrUN+jcUkJNPMuOA7QW+XUxAyM=",
 "url":"http://anifon.tk:7171/AgADFC769"
 } ]
