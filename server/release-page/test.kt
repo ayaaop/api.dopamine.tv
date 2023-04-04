51b13494cf782d0fa77e2deb74af8aa16c0817d4
dopamine-app <123301344+dopamine-app@users.noreply.github.com>
2023-04-04 10:47:03 +0600

    Update test.kt

--- server/release-page/test.kt
+++ server/release-page/test.kt
@@ -18,6 +18,28 @@
 "announcement":"null",
 "data":"null",
 "unavailable":"null",
-"id":"test"
-[{"description":"null1"},{"description":"null2"}]
+"id":"test",
+"description":"null1"
+},
+{"poster":"https://yt3.googleusercontent.com/GKvzT2Xh4xpZ7hszNcSPAAuvdJYJgYeUBGik0cjmR2JnnXHaO3smdKvImDqixd-xxQQjF22aynY=s900-c-k-c0x00ffffff-no-rj",
+"title":"Test Page",
+"title_orignal":"Test Page",
+"age":"0+",
+"country":"Қазақстан, 2023 жыл",
+"i_video":"0 бөлім. ~0 минут",
+"status":"Шықты",
+"studio":"Студия Dopamine Voice, автор Seele",
+"note":"",
+"info":"Сипаттама берілген жоқ!",
+"note_li":"null",
+"player":"null",
+"comments":"",
+"uncom":"null",
+"key":"test",
+"height":"5000",
+"announcement":"null",
+"data":"null",
+"unavailable":"null",
+"id":"test",
+"description":"null1"
 } ]
