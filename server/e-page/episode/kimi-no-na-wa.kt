a14112847d0d7ed4926050f1fefca0bbd9bd0a94
dopamine-app <123301344+dopamine-app@users.noreply.github.com>
2023-04-15 14:08:13 +0600

    Update kimi-no-na-wa.kt

--- server/e-page/episode/kimi-no-na-wa.kt
+++ server/e-page/episode/kimi-no-na-wa.kt
@@ -1,5 +1,5 @@
 [ {
 "episode":"Фильм",
 "is":"https://video.sibnet.ru/shell.php?videoid=4754278&share=1",
-"url":""
+"url":"https://video.sibnet.ru/v/f70c5de132ca0bf2408e043e87bffb2f/4754278.mp4"
 } ]
