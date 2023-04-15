1603b47823fe9bc237ef37267ca4b8b8dbe236d1
dopamine-app <123301344+dopamine-app@users.noreply.github.com>
2023-04-15 19:58:53 +0600

    Update v1.kt

--- server/poster/v1.kt
+++ server/poster/v1.kt
@@ -1,5 +1,8 @@
 [ {
-"poster":"https://dopamine-app.github.io/api.dopamine.tv/server/img/dop2.png",
+"poster":"https://e1.pxfuel.com/desktop-wallpaper/271/627/desktop-wallpaper-death-note-poster-death-note.jpg",
+"id":"death-note"
+},
+{"poster":"https://dopamine-app.github.io/api.dopamine.tv/server/img/dop2.png",
 "id":"gekijouban-jujutsu-kaisen-0"
 },
 {"poster":"https://dopaminevoice.kz/img/puss.png",
