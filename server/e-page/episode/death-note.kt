0691c267a2efb78bc2a0a8d11784cfb3510e1755
dopamine-app <123301344+dopamine-app@users.noreply.github.com>
2023-04-28 13:39:52 +0600

    Update death-note.kt

--- server/e-page/episode/death-note.kt
+++ server/e-page/episode/death-note.kt
@@ -1,1 +1,5 @@
-
+[ {
+"episode":"1 бөлім",
+"is":"https://firebasestorage.googleapis.com/v0/b/dopamine-voice.appspot.com/o/%D0%90%D0%B6%D0%B0%D0%BB%20%D0%94%D3%99%D0%BF%D1%82%D0%B5%D1%80%D1%96%2F%D0%90%D0%B6%D0%B0%D0%BB%20%D0%94%D3%99%D0%BF%D1%82%D0%B5%D1%80%D1%96%201%20%D0%91%D3%A9%D0%BB%D1%96%D0%BC.mp4?alt=media&token=aeec808e-e661-40e3-bfa1-a3abe98827d8",
+"url":"https://firebasestorage.googleapis.com/v0/b/dopamine-voice.appspot.com/o/%D0%90%D0%B6%D0%B0%D0%BB%20%D0%94%D3%99%D0%BF%D1%82%D0%B5%D1%80%D1%96%2F%D0%90%D0%B6%D0%B0%D0%BB%20%D0%94%D3%99%D0%BF%D1%82%D0%B5%D1%80%D1%96%201%20%D0%91%D3%A9%D0%BB%D1%96%D0%BC.mp4?alt=media&token=aeec808e-e661-40e3-bfa1-a3abe98827d8"
+} ]
