7261194a74bfb2f1191974d84da4abca960833ca
dopamine-app <123301344+dopamine-app@users.noreply.github.com>
2023-05-01 10:18:41 +0600

    Update sponsor.kt

--- application/donuts/sponsor.kt
+++ application/donuts/sponsor.kt
@@ -11,16 +11,16 @@
 },
 {"avatars":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS55eh71pI3nH7C65pyQjvm2ZKvE-jbmWtCcg&usqp=CAU",
 "username":"Nina",
-"donut":"600 теңге",
+"donut":"\\unk\\no\\wn\\ теңге",
 "uid":"OX4veOIPKWWu1HeJ9g6Frhkll363"
 },
 {"avatars":"https://dopamine-app.github.io/api.dopamine.tv/application/users/avatars/static_avatars.png",
 "username":"SON1C",
-"donut":"600 теңге",
+"donut":"600 теңге\nУақыты аяқталды!",
 "uid":"reUn4beiAmWlxE9b8rncDuCjyC73"
 },
 {"avatars":"https://firebasestorage.googleapis.com/v0/b/dopamine-apps.appspot.com/o/avatars%2FISMQcM25qifNaiY7eSotfnZpTTD2.png?alt=media&token=4ad692dd-33a9-4874-bdb4-18846483d75d",
 "username":"ayatop2",
-"donut":"520 теңге\nУақыты аяқталды!",
+"donut":"600 теңге\nУақыты аяқталды!",
 "uid":"ISMQcM25qifNaiY7eSotfnZpTTD2"
 } ]
