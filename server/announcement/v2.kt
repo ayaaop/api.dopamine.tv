0eb26976101622387ed62093de6f9b18aab49044
dopamine-app <123301344+dopamine-app@users.noreply.github.com>
2023-06-13 12:49:37 +0600

    Update v2.kt

--- server/announcement/v2.kt
+++ server/announcement/v2.kt
@@ -3,6 +3,6 @@
   "username":"Seele",
   "message":"test",
   "message_li":"true",
-  "poster_li":"true",
+  "poster_li":"false",
   "poster":"https://depor.com/resizer/DUnLwHWS6LYbvqNMSqxH8p3AqFY=/580x330/smart/filters:format(jpeg):quality(75)/cloudfront-us-east-1.images.arcpublishing.com/elcomercio/INO2R6UTI5FQFFJ3DW4BKBZCPE.jpg"
 } ]
