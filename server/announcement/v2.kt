c66c0ff2ec488136085d30dac513801e7c591494
dopamine-app <123301344+dopamine-app@users.noreply.github.com>
2023-06-13 14:51:49 +0600

    Update v2.kt

--- server/announcement/v2.kt
+++ server/announcement/v2.kt
@@ -1,5 +1,14 @@
 [ {
-  "avatars":"https://cdn.kanobu.ru/editor/images/53/e7f2a92d-a4c1-43a6-b4a3-62dde525299b.webp",
+  "avatars":"https://img.freepik.com/premium-vector/young-girl-anime-style-character-vector-illustration-design-manga-anime-girl_147933-93.jpg",
+  "username":"Anonymous",
+  "is_sponsor":"false",
+  "is_verfied":"false",
+  "message":"Hello World!",
+  "poster":"https://img.freepik.com/premium-vector/young-girl-anime-style-character-vector-illustration-design-manga-anime-girl_147933-93.jpg",
+  "message_li":"true",
+  "poster_li":"false"
+  },
+  {"avatars":"https://cdn.kanobu.ru/editor/images/53/e7f2a92d-a4c1-43a6-b4a3-62dde525299b.webp",
   "username":"Seele",
   "is_sponsor":"true",
   "is_verified":"true",
