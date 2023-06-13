607f154281dcec228c8809b785f4f32869e36014
dopamine-app <123301344+dopamine-app@users.noreply.github.com>
2023-06-13 14:22:10 +0600

    Create v2.kt

---
+++ server/announcement/v2.kt
@@ -0,0 +1,10 @@
+[ {
+  "avatars":"https://cdn.kanobu.ru/editor/images/53/e7f2a92d-a4c1-43a6-b4a3-62dde525299b.webp",
+  "username":"Seele",
+  "is_sponsor":"true",
+  "is_verified":"true",
+  "message":"Test",
+  "poster":"https://cdn.kanobu.ru/editor/images/53/e7f2a92d-a4c1-43a6-b4a3-62dde525299b.webp",
+  "message_li":"true",
+  "poster_li":"true"
+} ]
