fabd748f04a4e98f94dc6468c639a711c2319eb4
dopamine-app <123301344+dopamine-app@users.noreply.github.com>
2023-06-13 19:01:21 +0600

    Update v2.kt

--- server/announcement/v2.kt
+++ server/announcement/v2.kt
@@ -4,6 +4,6 @@
   "message_li":"true",
   "poster_li":"true",
   "key":"news-1",
-  "poster":"https://img.freepik.com/premium-vector/young-girl-anime-style-character-vector-illustration-design-manga-anime-girl_147933-93.jpg",
+  "news_poster":"https://img.freepik.com/premium-vector/young-girl-anime-style-character-vector-illustration-design-manga-anime-girl_147933-93.jpg",
   "title":"Test"
 } ]
