19ef65cbf5e0ccc9ab9d75bec807f58491a056ca
dopamine-app <123301344+dopamine-app@users.noreply.github.com>
2023-06-15 19:53:59 +0600

    Create bungou-stray-dogs.kt

---
+++ server/release-page/bungou-stray-dogs.kt
@@ -0,0 +1,47 @@
+[ {
+
+"poster":"https://static.anixart.tv/posters/Ofp2mTK2xWhDNHMGCogbOXAtrB9Rhq.jpg",
+
+"title":"Құдіретті қаңғыбас төбеттер",
+
+"title_orignal":"Bungou Stray Dogs",
+
+"age":"18+",
+
+"works":"japan",
+
+"country":"Жапония, көктем 2016 жыл",
+
+"i_video":"1/12 бөлім. ~24 минут",
+
+"status":"Көп бөлімді, шықты",
+
+"studio":"Студия Bones, автор Асагири Кафка",
+
+"note":"null",
+
+"note_li":"null",
+
+"info":"Әңгіме үлкен күштерге ие және бұл күштерді әртүрлі мақсаттарда, соның ішінде бизнесті жүргізу, жұмбақтарды шешу және тапсырмаларды орындау үшін пайдаланатын адамдар туралы. Сюжет негізінен «Қарулы Детектив Агенттігінің» мүшелері мен олардың күнделікті өміріне бағытталған.",
+
+"unavailable":"null",
+
+"comments":"null",
+
+"height":"null",
+
+"description":"фэнтези, экшн, хоррор",
+
+"uncom":"null",
+
+"announcement":"null",
+
+"premium":"null",
+
+"data":"null",
+
+"key":"bungou-stray-dogs",
+
+"id":"bungou-stray-dogs"
+
+} ]
