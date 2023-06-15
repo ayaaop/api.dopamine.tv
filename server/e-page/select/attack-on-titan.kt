487d6a99b3ecde5e8151573aeda02eb7f3d61935
dopamine-app <123301344+dopamine-app@users.noreply.github.com>
2023-06-15 21:12:55 +0600

    Create attack-on-titan.kt

---
+++ server/e-page/select/attack-on-titan.kt
@@ -0,0 +1,6 @@
+[ {
+  "studio_avatars":"https://dopamine-app.github.io/api.dopamine.tv/server/studio-logo/dopamine.voice-logo.png",
+  "studioName":"Dopamine Voice",
+  "episodeList":"12 бөлім",
+  "id":"attack-on-titan"
+} ]
