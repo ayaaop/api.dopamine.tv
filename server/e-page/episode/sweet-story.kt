0d047234c0e9737a2ac0d2bb2daf512f0de5a67b
dopamine-app <123301344+dopamine-app@users.noreply.github.com>
2023-06-16 00:00:50 +0600

    Create sweet-story.kt

---
+++ server/e-page/episode/sweet-story.kt
@@ -0,0 +1,25 @@
+[ {
+
+"episode":"1 бөлім",
+
+"is":"https://vk.com/video_ext.php?oid=-112940307&id=171747180&hash=5d11b801d2a9b998",
+
+"url":"https://vk.com/video_ext.php?oid=-112940307&id=171747180&hash=5d11b801d2a9b998"
+
+},
+
+{"episode":"2 бөлім",
+
+"is":"https://vk.com/video_ext.php?oid=-112940307&id=171754011&hash=feece46aef2ad7bb",
+
+"url":"https://vk.com/video_ext.php?oid=-112940307&id=171754011&hash=feece46aef2ad7bb"
+
+},
+
+{"episode":"3 бөлім",
+
+"is":"https://vk.com/video_ext.php?oid=-112940307&id=171754295&hash=f119bca2fccf7c0c",
+
+"url":"https://vk.com/video_ext.php?oid=-112940307&id=171754295&hash=f119bca2fccf7c0c"
+
+} ]
