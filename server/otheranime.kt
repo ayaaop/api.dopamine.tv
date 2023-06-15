8ac995be2dff21d1e1b6a9bfda4e065f43001034
dopamine-app <123301344+dopamine-app@users.noreply.github.com>
2023-06-15 22:01:50 +0600

    Update otheranime.kt

--- server/otheranime.kt
+++ server/otheranime.kt
@@ -5,11 +5,4 @@
   "rating":"1/12",
   "info":"Әңгіме үлкен күштерге ие және бұл күштерді әртүрлі мақсаттарда, соның ішінде бизнесті жүргізу, жұмбақтарды шешу және тапсырмаларды орындау үшін пайдаланатын адамдар туралы. Сюжет негізінен «Қарулы Детектив Агенттігінің» мүшелері мен олардың күнделікті өміріне бағытталған.",
   "id":"bungou-stray-dogs"
-  },
-  {"poster":"https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcQU3JHYjZuIJBsc27EqAHqqLHUVq9f6f1NT5EWe5aczB8IbM-WP",
-  "title":"Хз что писать",
-  "episode":"1/12",
-  "rating":"1/12",
-  "info":"com.dopaminevoice.kz.SYSTEM.SEELE",
-  "id":"test"
 } ]
