ac47507a3c8b34b47b6613b98014764d519eb88e
dopamine-app <123301344+dopamine-app@users.noreply.github.com>
2023-06-15 21:20:42 +0600

    Create death-note.kt

---
+++ server/e-page/select/death-note.kt
@@ -0,0 +1,11 @@
+[ {
+
+"studio_avatars":"https://dopamine-app.github.io/api.dopamine.tv/server/studio-logo/dopamine.voice-logo.png",
+
+"studioName":"Dopamine Voice",
+
+"episodeList":"1 бөлім",
+
+"id":"death-note."
+
+} ]
