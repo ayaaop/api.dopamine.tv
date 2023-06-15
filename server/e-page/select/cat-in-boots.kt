412ca04c6b32fe36fc16370946453b847b978790
dopamine-app <123301344+dopamine-app@users.noreply.github.com>
2023-06-15 21:15:41 +0600

    Create cat-in-boots.kt

---
+++ server/e-page/select/cat-in-boots.kt
@@ -0,0 +1,6 @@
+[ {
+  "studio_avatars":"api.dopamine.tv/server/studio-logo/dopamine.voice-logo.png",
+  "studioName":"Dopamine Voice",
+  "episodeList":"Толықметражды",
+  "id":"cat-in-boots.kt"
+} ]
