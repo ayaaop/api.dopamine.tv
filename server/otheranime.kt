e2140d3be0a9c8c11302d9f78d388997f3ec1dc3
dopamine-app <123301344+dopamine-app@users.noreply.github.com>
2023-06-15 18:51:31 +0600

    Update otheranime.kt

--- server/otheranime.kt
+++ server/otheranime.kt
@@ -1,1 +1,8 @@
-
+[ {
+  "poster":"https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcQU3JHYjZuIJBsc27EqAHqqLHUVq9f6f1NT5EWe5aczB8IbM-WP",
+  "title":"Хз что писать",
+  "episode":"1/12",
+  "rating":"1/12",
+  "info":"com.dopaminevoice.kz.SYSTEM.SEELE",
+  "id":"test",
+} ]
