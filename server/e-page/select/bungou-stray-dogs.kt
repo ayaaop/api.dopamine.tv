aa88947c8211a3c960cb46f63568a1d1b11972d0
dopamine-app <123301344+dopamine-app@users.noreply.github.com>
2023-06-15 21:13:30 +0600

    Update bungou-stray-dogs.kt

--- server/e-page/select/bungou-stray-dogs.kt
+++ server/e-page/select/bungou-stray-dogs.kt
@@ -1,6 +1,6 @@
 [ {
   "studio_avatars":"https://dopamine-app.github.io/api.dopamine.tv/server/studio-logo/ayba.dybys-logo.png",
   "studioName":"Aiba.Dub",
-  "episodeList":"1 эпизод",
+  "episodeList":"1 бөлім",
   "id":"bungou-stray-dogs"
 } ]
