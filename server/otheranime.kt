9b9481573322985e2ebf5aea4a24792e1d139079
dopamine-app <123301344+dopamine-app@users.noreply.github.com>
2023-06-15 19:56:04 +0600

    Update otheranime.kt

--- server/otheranime.kt
+++ server/otheranime.kt
@@ -1,5 +1,12 @@
 [ {
-  "poster":"https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcQU3JHYjZuIJBsc27EqAHqqLHUVq9f6f1NT5EWe5aczB8IbM-WP",
+  "poster":"https://static.anixart.tv/posters/Ofp2mTK2xWhDNHMGCogbOXAtrB9Rhq.jpg",
+  "title":"Құдіретті қаңғыбас төбеттер",
+  "episode":"1/12",
+  "rating":"1/12",
+  "info":"Әңгіме үлкен күштерге ие және бұл күштерді әртүрлі мақсаттарда, соның ішінде бизнесті жүргізу, жұмбақтарды шешу және тапсырмаларды орындау үшін пайдаланатын адамдар туралы. Сюжет негізінен «Қарулы Детектив Агенттігінің» мүшелері мен олардың күнделікті өміріне бағытталған.",
+  "id":"bungou-stray-dogs"
+  },
+  {"poster":"https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcQU3JHYjZuIJBsc27EqAHqqLHUVq9f6f1NT5EWe5aczB8IbM-WP",
   "title":"Хз что писать",
   "episode":"1/12",
   "rating":"1/12",
