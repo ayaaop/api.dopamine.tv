36f4b26deae522b6700c167506342ba6ce2c710a
dopamine-app <123301344+dopamine-app@users.noreply.github.com>
2023-06-15 20:56:25 +0600

    Create bungou-stray-dogs.kt

---
+++ server/e-page/select/bungou-stray-dogs.kt
@@ -0,0 +1,6 @@
+[ {
+  "studio_avatars":"https://dopamine-app.github.io/api.dopamine.tv/server/studio-logo/ayba.dybys-logo.png",
+  "studioName":"Aiba.Dub",
+  "episodeList":"1 эпизод",
+  "id":"bungou-stray-dogs"
+} ]
