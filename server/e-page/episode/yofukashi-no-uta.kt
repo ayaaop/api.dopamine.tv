8eba60d9c0474fd940b5554a32d14e4fcc19f113
dopamine-app <123301344+dopamine-app@users.noreply.github.com>
2023-06-16 18:58:14 +0600

    Create yofukashi-no-uta.kt

---
+++ server/e-page/episode/yofukashi-no-uta.kt
@@ -0,0 +1,89 @@
+[ {
+
+"episode":"1 бөлім",
+
+"is":"https://video.sibnet.ru/shell.php?videoid=4956156",
+
+"url":"https://video.sibnet.ru/shell.php?videoid=4956156"
+
+},
+
+{"episode":"2 бөлім",
+
+"is":"https://video.sibnet.ru/shell.php?videoid=4957086",
+
+"url":"https://video.sibnet.ru/shell.php?videoid=4957086"
+
+},
+
+{"episode":"3 бөлім",
+
+"is":"https://video.sibnet.ru/shell.php?videoid=4957082",
+
+"url":"https://video.sibnet.ru/shell.php?videoid=4957082"
+
+},
+
+{"episode":"4 бөлім",
+
+"is":"https://video.sibnet.ru/shell.php?videoid=4957070",
+
+"url":"https://video.sibnet.ru/shell.php?videoid=4957070"
+
+},
+
+{"episode":"5 бөлім",
+
+"is":"https://video.sibnet.ru/shell.php?videoid=4957088",
+
+"url":"https://video.sibnet.ru/shell.php?videoid=4957088"
+
+},
+
+{"episode":"6 бөлім",
+
+"is":"https://video.sibnet.ru/shell.php?videoid=4957025",
+
+"url":"https://video.sibnet.ru/shell.php?videoid=4957025"
+
+},
+
+{"episode":"7 бөлім",
+
+"is":"https://video.sibnet.ru/shell.php?videoid=4957045",
+
+"url":"https://video.sibnet.ru/shell.php?videoid=4957045"
+
+},
+
+{"episode":"8 бөлім",
+
+"is":"https://video.sibnet.ru/shell.php?videoid=4957061",
+
+"url":"https://video.sibnet.ru/shell.php?videoid=4957061"
+
+},
+
+{"episode":"9 бөлім",
+
+"is":"https://video.sibnet.ru/shell.php?videoid=5036850",
+
+"url":"https://video.sibnet.ru/shell.php?videoid=5036850"
+
+},
+
+{"episode":"10 бөлім",
+
+"is":"https://video.sibnet.ru/shell.php?videoid=5039162",
+
+"url":"https://video.sibnet.ru/shell.php?videoid=5039162"
+
+},
+
+{"episode":"11 бөлім",
+
+"is":"https://video.sibnet.ru/shell.php?videoid=5039204",
+
+"url":"https://video.sibnet.ru/shell.php?videoid=5039204"
+
+} ]
