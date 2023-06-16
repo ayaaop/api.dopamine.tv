60181ef745240883123a5029d1dcc31d5890142e
dopamine-app <123301344+dopamine-app@users.noreply.github.com>
2023-06-16 15:59:26 +0600

    Create one-piece.kt

---
+++ server/e-page/select/one-piece.kt
@@ -0,0 +1,11 @@
+[ {
+
+"studio_avatars":"https://dopamine-app.github.io/api.dopamine.tv/application/users/avatars/static_avatars.png",
+
+"studioName":"RamazanSub | Субтитры",
+
+"episodeList":"1 бөлім",
+
+"id":"one-piece"
+
+} ]
