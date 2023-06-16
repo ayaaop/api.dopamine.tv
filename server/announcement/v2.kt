d43e8ee04f6771f90ddb413b87032c60d93988ce
dopamine-app <123301344+dopamine-app@users.noreply.github.com>
2023-06-16 13:46:42 +0600

    Update v2.kt

--- server/announcement/v2.kt
+++ server/announcement/v2.kt
@@ -1,13 +1,5 @@
 [ {
-  "message":"Сәлем достар👋🏻\nБіздің қосымшадан қай студияный дыбыстамаларын көргіңіз келетін еді? Пікіріңізді қалдыра кетіңіз🗨️",
-  "poster":"null",
-  "message_li":"true",
-  "poster_li":"false",
-  "key":"news-2",
-  "news_poster":"https://anime.atsit.in/no/wp-content/uploads/2022/07/hvilken-anime-er-anya-fra-hvorfor-er-hun-sa-kjent.jpg",
-  "title":"Сәлем 🙌🏻"
-  },
-  {"message":"Test 0",
+"message":"Test 0",
   "poster":"https://img.freepik.com/premium-vector/young-girl-anime-style-character-vector-illustration-design-manga-anime-girl_147933-93.jpg",
   "message_li":"true",
   "poster_li":"true",
