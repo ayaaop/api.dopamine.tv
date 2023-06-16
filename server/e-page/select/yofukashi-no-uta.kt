bc1e7da62e20574669f8a4477cc8b1253d70d768
dopamine-app <123301344+dopamine-app@users.noreply.github.com>
2023-06-16 18:19:33 +0600

    Create yofukashi-no-uta.kt

---
+++ server/e-page/select/yofukashi-no-uta.kt
@@ -0,0 +1,11 @@
+[ {
+
+"studio_avatars":"https://dopamine-app.github.io/api.dopamine.tv/server/studio-logo/qaz.anime-logo.png",
+
+"studioName":"QazAnime",
+
+"episodeList":"11 бөлім",
+
+"id":"yofukashi-no-uta"
+
+} ]
