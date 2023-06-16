004fae0776c3caee6dede74554015eedca2cb10f
dopamine-app <123301344+dopamine-app@users.noreply.github.com>
2023-06-16 18:15:20 +0600

    Create yofukashi-no-uta.kt

---
+++ server/release-page/yofukashi-no-uta.kt
@@ -0,0 +1,49 @@
+[ {
+
+"poster":"https://avatars.mds.yandex.net/get-kinopoisk-image/6201401/928093b3-e31f-47e8-b3b3-ce93588ee675/440x660",
+
+"title":"Түн қоңырауы",
+
+"title_orignal":"Yofukashi no Uta",
+
+"age":"18+",
+
+"works":"japan",
+
+"country":"Жапония, жаз 2022 жыл",
+
+"i_video":"13 бөлім. ~24 минут",
+
+"status":"Көп бөлімді, шықты",
+
+"studio":"Студия LIDENFILMS, автор Kotoyama",
+
+"available_player":"Қолжетімді бейне ойнатқыш: Web-Player",
+
+"note":"null",
+
+"note_li":"null",
+
+"info":"Бейтаныс адаммен кездейсоқ кездесу Ямори Ко деген атты 8 сынып оқушысын түнгі өмірмен таныстырады. Ко оның соңынан ереді.",
+
+"unavailable":"null",
+
+"comments":"null",
+
+"height":"null",
+
+"key":"yofukashi-no-uta",
+
+"premium":"null",
+
+"description":"романтика, сёнэн, вампирлер, тылсым күштер",
+
+"uncom":"null",
+
+"announcement":"null",
+
+"data":"null",
+
+"id":"yofukashi-no-uta"
+
+} ]
