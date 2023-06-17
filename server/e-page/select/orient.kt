c16603e66a7739636f682da80d6852790b4c34ad
dopamine-app <123301344+dopamine-app@users.noreply.github.com>
2023-06-17 13:12:17 +0600

    Create orient.kt

---
+++ server/e-page/select/orient.kt
@@ -0,0 +1,11 @@
+[ {
+
+  "studio_avatars":"https://dopamine-app.github.io/api.dopamine.tv/server/studio-logo/ayba.dybys-logo.png",
+
+  "studioName":"Aiba.Dub",
+
+  "episodeList":"4 бөлім",
+
+  "id":"orient"
+
+} ]
