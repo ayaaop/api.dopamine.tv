16a29153ba80036f153a834d7687aa35dcc6201d
dopamine-app <123301344+dopamine-app@users.noreply.github.com>
2023-06-17 15:52:13 +0600

    Create demon-slayer.kt

---
+++ server/release-page/demon-slayer.kt
@@ -0,0 +1,49 @@
+[ {
+
+"poster":"https://m.media-amazon.com/images/I/71QA1m0KGKL._AC_UF894,1000_QL80_.jpg",
+
+"title":"Жын жоюшы",
+
+"title_orignal":"Demon Slayer",
+
+"age":"18+",
+
+"works":"japan",
+
+"country":"Жапония, көктем 2019 жыл",
+
+"i_video":"6/26 бөлім. ~24 минут",
+
+"status":"Көп бөлімді, шықты",
+
+"studio":"Студия Ufotable, автор Коёхару Готогэ",
+
+"available_player":"Қолжетімді бейне ойнатқыш: Web-Player",
+
+"note":"null",
+
+"note_li":"null",
+
+"info":"Танжиро – асыраушысынан айырылған отбасының үлкен ұлы. Бір күні ол басқа қалаға көмір сатуға барады, бірақ үйіне бармай, бейтаныс үйде түнеп қалады. Мұның бәрі түн жамылып тауда жүрген жын туралы қауесетке байланысты. Таңертең жігіт үйіне аман-есен оралады... Бірақ оны қорқынышты жаңалық күтіп тұр…",
+
+"unavailable":"null",
+
+"comments":"null",
+
+"height":"null",
+
+"key":"demon-slayer",
+
+"premium":"null",
+
+"description":"шытырман оқиғалар, тарихи, тылсым күштер",
+
+"uncom":"null",
+
+"announcement":"null",
+
+"data":"null",
+
+"id":"demon-slayer"
+
+} ]
