9dbbe6861f5dbd63b1c2a1147176098aac860efb
dopamine-app <123301344+dopamine-app@users.noreply.github.com>
2023-06-17 12:53:32 +0600

    Create vanitas-no-carte.kt

---
+++ server/release-page/vanitas-no-carte.kt
@@ -0,0 +1,49 @@
+[ {
+
+"poster":"https://static.anixart.tv/posters/I1lKSp5kHl4bf4nmNZsdnsQYFGPBS8.jpg",
+
+"title":"Ванитастың Мұрасы",
+
+"title_orignal":"Vanitas no Carte",
+
+"age":"18+",
+
+"works":"japan",
+
+"country":"Жапония, жаз 2021 жыл",
+
+"i_video":"12 бөлім. ~24 минут",
+
+"status":"Көп бөлімді, шықты",
+
+"studio":"Студия Bones, автор Дзюн Мотидзуки",
+
+"available_player":"Қолжетімді бейне ойнатқыш: Web-Player",
+
+"note":"null",
+
+"note_li":"null",
+
+"info":"Сипаттама берілген жоқ",
+
+"unavailable":"null",
+
+"comments":"null",
+
+"height":"null",
+
+"key":"vanitas-no-carte",
+
+"premium":"null",
+
+"description":"тарихий, сёнен, фэнтези, вампирлер, тылсым күштер",
+
+"uncom":"null",
+
+"announcement":"null",
+
+"data":"null",
+
+"id":"vanitas-no-carte"
+
+} ]
