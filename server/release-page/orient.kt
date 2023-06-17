9fbdac4c269256392ad54b8a48a2eb627f0df51d
dopamine-app <123301344+dopamine-app@users.noreply.github.com>
2023-06-17 13:11:19 +0600

    Create orient.kt

---
+++ server/release-page/orient.kt
@@ -0,0 +1,49 @@
+[ {
+
+"poster":"https://static.anixart.tv/posters/TMsYbS5ngAFULSnorjw9TIKh1tEt2I.jpg",
+
+"title":"Ориент",
+
+"title_orignal":"Orient",
+
+"age":"16+",
+
+"works":"japan",
+
+"country":"Жапония, қыс 2022 жыл",
+
+"i_video":"4/12 бөлім. ~24 минут",
+
+"status":"Көп бөлімді, шықты",
+
+"studio":"Студия A.C.G.T, автор Синобу Отака",
+
+"available_player":"Қолжетімді бейне ойнатқыш: Web-Player",
+
+"note":"null",
+
+"note_li":"null",
+
+"info":"Көптеген жылдар бойы феодалдар Хиномотода қанды соғыс жүргізді. Бірде оларға жұмбақ Кисин шабуыл жасады, ол феодалдарды әдіспен жоюды бастады. Бұл жын адамдардың Жер бетінен жоғалып кетуін қалайды. Содан бері бір жарым ғасыр өтті. Кисинді өлтіруді шешкен Буши жауынгерлері пайда болды.",
+
+"unavailable":"null",
+
+"comments":"null",
+
+"height":"null",
+
+"key":"orient",
+
+"premium":"null",
+
+"description":"экшен, сёнен, фэнтези",
+
+"uncom":"null",
+
+"announcement":"null",
+
+"data":"null",
+
+"id":"orient"
+
+} ]
