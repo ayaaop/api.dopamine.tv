8bc046dceef81c181b31167ebd93ae38df3127b9
dopamine-app <123301344+dopamine-app@users.noreply.github.com>
2023-06-17 12:59:40 +0600

    Create vanitas-no-carte.kt

---
+++ server/e-page/select/vanitas-no-carte.kt
@@ -0,0 +1,6 @@
+[ {
+  "studio_avatars":"https://dopamine-app.github.io/api.dopamine.tv/server/studio-logo/special.production-logo.png",
+  "studioName":"Special Production",
+  "episodeList":"1 бөлім",
+  "id":"vanitas-no-carte"
+} ]
