a35e0504b67f85b5f1517cbacb6cccc6a5832625
dopamine-app <123301344+dopamine-app@users.noreply.github.com>
2023-06-17 12:25:54 +0600

    Update bungou-stray-dogs.kt

--- server/e-page/episode/bungou-stray-dogs.kt
+++ server/e-page/episode/bungou-stray-dogs.kt
@@ -2,4 +2,8 @@
   "episode":"1 бөлім",
   "is":"https://ok.ru/videoembed/5330691033819",
   "url":"https://ok.ru/videoembed/5330691033819"
+  },
+ {"episode":"2 бөлім",
+  "is":"https://ok.ru/videoembed/5336574855899",
+  "url":"https://ok.ru/videoembed/5336574855899"
 } ]
