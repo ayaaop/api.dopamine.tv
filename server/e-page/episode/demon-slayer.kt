f80b33c05ea3dd2f39140dd012712ba8b9df3532
dopamine-app <123301344+dopamine-app@users.noreply.github.com>
2023-06-17 16:03:02 +0600

    Create demon-slayer.kt

---
+++ server/e-page/episode/demon-slayer.kt
@@ -0,0 +1,49 @@
+[ {
+
+"episode":"1 бөлім",
+
+"is":"https://video.sibnet.ru/shell.php?videoid=5055502",
+
+"url":"https://video.sibnet.ru/shell.php?videoid=5055502"
+
+},
+
+{"episode":"2 бөлім",
+
+"is":"https://video.sibnet.ru/shell.php?videoid=5055468",
+
+"url":"https://video.sibnet.ru/shell.php?videoid=5055468"
+
+},
+
+{"episode":"3 бөлім",
+
+"is":"https://video.sibnet.ru/shell.php?videoid=5055422",
+
+"url":"https://video.sibnet.ru/shell.php?videoid=5055422"
+
+},
+
+{"episode":"4 бөлім",
+
+"is":"https://video.sibnet.ru/shell.php?videoid=5055392",
+
+"url":"https://video.sibnet.ru/shell.php?videoid=5055392"
+
+},
+
+{"episode":"5 бөлім",
+
+"is":"https://video.sibnet.ru/shell.php?videoid=5055305",
+
+"url":"https://video.sibnet.ru/shell.php?videoid=5055305"
+
+},
+
+{"episode":"6 бөлім",
+
+"is":"https://video.sibnet.ru/shell.php?videoid=5064373",
+
+"url":"https://video.sibnet.ru/shell.php?videoid=5064373"
+
+} ]
