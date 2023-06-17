ef603ff7dfd4cde1115543872f4823cf7261023f
dopamine-app <123301344+dopamine-app@users.noreply.github.com>
2023-06-17 15:56:12 +0600

    Create demon-slayer.kt

---
+++ server/e-page/select/demon-slayer.kt
@@ -0,0 +1,6 @@
+[ {
+  "studio_avatars":"https://dopamine-app.github.io/api.dopamine.tv/server/studio-logo/ani.dybys-logo.png",
+  "studioName":"AniDybys",
+  "episodeList":"6 бөлім",
+  "id":"demon-slayer"
+} ]
