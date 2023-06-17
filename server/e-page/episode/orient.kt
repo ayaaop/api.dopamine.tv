05057123877801e3fc92902c098cff30e0c6b703
dopamine-app <123301344+dopamine-app@users.noreply.github.com>
2023-06-17 13:20:54 +0600

    Create orient.kt

---
+++ server/e-page/episode/orient.kt
@@ -0,0 +1,33 @@
+[ {
+
+"episode":"1 бөлім",
+
+"is":"https://ok.ru/videoembed/5335730621147",
+
+"url":"https://ok.ru/videoembed/5335730621147"
+
+},
+
+{"episode":"2 бөлім",
+
+"is":"https://ok.ru/videoembed/5335958031067",
+
+"url":"https://ok.ru/videoembed/5335958031067"
+
+},
+
+{"episode":"3 бөлім",
+
+"is":"https://ok.ru/videoembed/5336387488475",
+
+"url":"https://ok.ru/videoembed/5336387488475"
+
+},
+
+{"episode":"4 бөлім",
+
+"is":"https://ok.ru/videoembed/5336463575771",
+
+"url":"https://ok.ru/videoembed/5336463575771"
+
+} ]
