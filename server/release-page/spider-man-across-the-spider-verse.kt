457d08f7bdfd6fcdf3ed3946e866042ebc40c166
dopamine-app <123301344+dopamine-app@users.noreply.github.com>
2023-06-19 20:54:49 +0600

    Create spider-man-across-the-spider-verse.kt

---
+++ server/release-page/spider-man-across-the-spider-verse.kt
@@ -0,0 +1,49 @@
+[ {
+
+"poster":"https://dopamine-app.github.io/api.dopamine.tv/server/img/spider-man-across-the-spider-verse.jpg",
+
+"title":"Өрмекші-адам: Ғаламдар торы",
+
+"title_orignal":"Spider-Man: Across the Spider-Verse",
+
+"age":"6+",
+
+"works":"japan",
+
+"country":"АҚШ, 2023 жыл",
+
+"i_video":"1 бөлім. ~2 сағат 20 минут",
+
+"status":"Толықметражды, шықты",
+
+"studio":"Студия Marvel Comics",
+
+"available_player":"Қолжетімді бейне ойнатқыш: Web-Player",
+
+"note":"null",
+
+"note_li":"null",
+
+"info":"Майлз Моралес суперқаһарман киімін киіп, Нью-Йоркті қылмыскерлерден құтқарады. Бірақ құтқарушының жауапкершілігі кейде оның өміріне кедергі жасайды, сондықтан Өрмекші адам отбасында шиеленіс пайда болады. Кейіпкер «Дақпен» кездескенде, бәрі өзгеріп кетеді, оған қоса, Моралестің ғашығы, Гвен Стейси басқа әлемнен оралып, өрмекшіні жаңа шытырман оқиғаға жетелегенде, жағдай нашарлайды.",
+
+"player":"null",
+
+"unavailable":"null",
+
+"premium":"null",
+
+"comments":"null",
+
+"key":"sen-to-chihiro-no-kamikakushi",
+
+"height":"null",
+
+"uncom":"null",
+
+"id":"spider-man-across-the-spider-verse",
+
+"data":"Жуырда.",
+
+"description":"шытырман оқиғалар"
+
+} ]
