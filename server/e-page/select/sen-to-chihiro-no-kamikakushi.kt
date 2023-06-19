22b0a422c1f3745e7e5896d51459f6df125ca96c
dopamine-app <123301344+dopamine-app@users.noreply.github.com>
2023-06-19 20:40:17 +0600

    Create sen-to-chihiro-no-kamikakushi.kt

---
+++ server/e-page/select/sen-to-chihiro-no-kamikakushi.kt
@@ -0,0 +1,6 @@
+[ {
+  "studio_avatars":"https://dopamine-app.github.io/api.dopamine.tv/server/studio-logo/ani.dybys-logo.png",
+  "studioName":"Dopamine Voice",
+  "episodeList":"Толықметражды",
+  "id":"sen-to-chihiro-no-kamikakushi"
+} ]
