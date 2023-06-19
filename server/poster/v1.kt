4a1d468413b0c7954c7e49796975bfbe89c64aae
dopamine-app <123301344+dopamine-app@users.noreply.github.com>
2023-06-19 21:38:18 +0600

    Update v1.kt

--- server/poster/v1.kt
+++ server/poster/v1.kt
@@ -1,9 +1,9 @@
 [ {
-"poster":"https://e1.pxfuel.com/desktop-wallpaper/271/627/desktop-wallpaper-death-note-poster-death-note.jpg",
-"id":"death-note"
-},
-{"poster":"https://rus.team/images/article/44173/2018-12-27-467_75641-1_261290.webp",
+"poster":"https://dopamine-app.github.io/api.dopamine.tv/server/i/spirited_banner2.png",
 "id":"sen-to-chihiro-no-kamikakushi"
+},
+{"poster":"https://e1.pxfuel.com/desktop-wallpaper/271/627/desktop-wallpaper-death-note-poster-death-note.jpg",
+"id":"death-note"
 },
 {"poster":"https://dopamine-app.github.io/api.dopamine.tv/server/img/dop2.png",
 "id":"gekijouban-jujutsu-kaisen-0"
