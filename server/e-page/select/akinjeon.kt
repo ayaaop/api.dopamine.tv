9f60318688b0ab2d237c6aedcf39cb152cad08f6
dopamine-app <123301344+dopamine-app@users.noreply.github.com>
2023-06-21 15:09:31 +0600

    Create akinjeon.kt

---
+++ server/e-page/select/akinjeon.kt
@@ -0,0 +1,6 @@
+[ {
+  "studio_avatars":"https://static.tildacdn.com/tild3864-6135-4562-a534-376232373235/photo.png",
+  "studioName":"Q-FILMS",
+  "episodeList":"Толықметражды",
+  "id":"akinjeon"
+} ]
