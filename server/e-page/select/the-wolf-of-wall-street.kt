b12dff7657cecd4563b4c18842b6ff67f2a799b5
dopamine-app <123301344+dopamine-app@users.noreply.github.com>
2023-06-21 11:00:45 +0600

    Create the-wolf-of-wall-street.kt

---
+++ server/e-page/select/the-wolf-of-wall-street.kt
@@ -0,0 +1,6 @@
+[ {
+  "studio_avatars":"https://dopamine-app.github.io/api.dopamine.tv/server/img/dopamine.voice-logo.png",
+  "studioName":"Dopamine Studio",
+  "episodeList":"Толықметражды",
+  "id":"the-wolf-of-wall-street"
+} ]
