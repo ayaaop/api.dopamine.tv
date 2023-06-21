70f9585f3b01e33185b7d2c9e34f8e57d83b6bf0
dopamine-app <123301344+dopamine-app@users.noreply.github.com>
2023-06-21 14:39:40 +0600

    Update mojin-the-worm-valley.kt

--- server/e-page/episode/mojin-the-worm-valley.kt
+++ server/e-page/episode/mojin-the-worm-valley.kt
@@ -1,5 +1,5 @@
 [ {
   "episode":"Фильм",
   "is":"https://vk.com/video_ext.php?oid=-217173314&id=456239504&hash=2c583c0ca0405131",
-  "url":"https://vk.com/video_ext.php?oid=-217173314&id=456239504&hash=2c583c0ca0405131
+  "url":"https://vk.com/video_ext.php?oid=-217173314&id=456239504&hash=2c583c0ca0405131"
 } ]
