1c34174ffa869ee5ec69c9bebf0244f4c78a85a4
dopamine-app <123301344+dopamine-app@users.noreply.github.com>
2023-06-21 10:47:03 +0600

    Delete titanic.kt

--- server/e-page/select/titanic.kt
+++
@@ -1,6 +0,0 @@
-[ {
-  "studio_avatars":"https://play-lh.googleusercontent.com/XITZmvMGzAoCeqk6ruVkhupKy3bdhuKYqwIIZyII5QJvHmS5TQ7Bfbtktjt99fEukPw",
-  "studioName":"QAZAQSTAN TV",
-  "episodeList":"Толықметражды",
-  "id":"titanic"
-} ]
