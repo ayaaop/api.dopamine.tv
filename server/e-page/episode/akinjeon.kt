ba785ea1273fac20ace39626e24a7f206336ad12
dopamine-app <123301344+dopamine-app@users.noreply.github.com>
2023-06-21 15:10:44 +0600

    Create akinjeon.kt

---
+++ server/e-page/episode/akinjeon.kt
@@ -0,0 +1,5 @@
+[ {
+  "episode":"Фильм",
+  "is":"https://vk.com/video_ext.php?oid=-217173314&id=456239286&hash=03ac70faa3e3bc4b",
+  "url":"https://vk.com/video_ext.php?oid=-217173314&id=456239286&hash=03ac70faa3e3bc4b"
+} ]
