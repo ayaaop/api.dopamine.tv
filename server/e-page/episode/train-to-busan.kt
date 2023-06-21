01b287021415ec3c8c285ea1eb4dc7c55b9a01dd
dopamine-app <123301344+dopamine-app@users.noreply.github.com>
2023-06-21 11:28:12 +0600

    Create train-to-busan.kt

---
+++ server/e-page/episode/train-to-busan.kt
@@ -0,0 +1,5 @@
+[ {
+  "episode":"Толықметражды",
+  "is":"https://vk.com/video_ext.php?oid=-217173314&id=456239425&hash=21be85f3b6e6edd8",
+  "url":"https://vk.com/video_ext.php?oid=-217173314&id=456239425&hash=21be85f3b6e6edd8"
+} ]
