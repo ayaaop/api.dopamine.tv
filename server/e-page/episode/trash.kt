84a1dbf9c3aa53bc4b6f47da39e158c9c86c5619
dopamine-app <123301344+dopamine-app@users.noreply.github.com>
2023-06-23 08:49:28 +0600

    Create trash.kt

---
+++ server/e-page/episode/trash.kt
@@ -0,0 +1,5 @@
+[ {
+  "episode":"Фильм",
+  "is":"https://vk.com/video_ext.php?oid=-217173314&id=456239519&hash=df88b7e86f690675",
+  "url":"https://vk.com/video_ext.php?oid=-217173314&id=456239519&hash=df88b7e86f690675"
+} ]
