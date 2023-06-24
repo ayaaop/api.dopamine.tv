fa92c7e552d24b40ea2695adbeaf8d8fb7693907
dopamine-app <123301344+dopamine-app@users.noreply.github.com>
2023-06-24 16:02:43 +0600

    Create the-lord-of-the-rings.kt

---
+++ server/e-page/episode/the-lord-of-the-rings.kt
@@ -0,0 +1,13 @@
+[ {
+  "episode":"Фильм «2001»",
+  "is":"https://vk.com/video_ext.php?oid=-217173314&id=456239310&hash=2536790ad7fcb64b",
+  "url":"https://vk.com/video_ext.php?oid=-217173314&id=456239310&hash=2536790ad7fcb64b"
+  },
+  {"episode":"Фильм «2002»",
+  "is":"https://vk.com/video_ext.php?oid=-217173314&id=456239311&hash=03ac7ca69ec1cd9e",
+  "url":"https://vk.com/video_ext.php?oid=-217173314&id=456239311&hash=03ac7ca69ec1cd9e"
+  },
+  {"episode":"Фильм «2003»",
+  "is":"https://vk.com/video_ext.php?oid=-217173314&id=456239312&hash=0a3468b6b99b9788",
+  "url":"https://vk.com/video_ext.php?oid=-217173314&id=456239312&hash=0a3468b6b99b9788"
+} ]
