b142d5f84456b68b3c9be7110c98a16f7cb2e838
dopamine-app <123301344+dopamine-app@users.noreply.github.com>
2023-06-24 16:03:59 +0600

    Create the-lord-of-the-rings.kt

---
+++ server/e-page/select/the-lord-of-the-rings.kt
@@ -0,0 +1,6 @@
+[ {
+  "studio_avatars":"https://play-lh.googleusercontent.com/XITZmvMGzAoCeqk6ruVkhupKy3bdhuKYqwIIZyII5QJvHmS5TQ7Bfbtktjt99fEukPw",
+  "studioName":"QAZAQSTAN TV",
+  "episodeList":"3 бөлім",
+  "id":"the-lord-of-the-rings"
+} ]
