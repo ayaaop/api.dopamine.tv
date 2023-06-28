bb9bc3a518aa0f5de70204d771aa145851969cb0
dopamine-app <123301344+dopamine-app@users.noreply.github.com>
2023-06-28 21:55:58 +0600

    Update test.kt

--- server/release-page/test.kt
+++ server/release-page/test.kt
@@ -1,39 +1,24 @@
 [ {
-"poster":"https://static.hdrezka.ac/i/2022/8/29/x431b482f131aqk30y58g.jpg",
-"title":"Сенің сәуірдегі жалған сөзің",
-"title_orignal":"Shigatsu wa kimi no uso",
-"age":"12+",
-"works":"china",
-"country":"Жапония, күз 2014 жыл",
-"i_video":"22 бөлім. ~22 минут",
-"status":"Көп бөлімді, шықты",
-"studio":"Студия A-1 Pictures, автор Наоси Аракава",
-"available_player":"",
-"note":"歌手: グース ハウス\n曲名: 光るなら\nアニメ「四月は君の嘘」オープニングテーマ1\n\n雨上がりの虹も　凛と咲いた花も　色づき溢れだす\n茜色の空仰ぐ君に　あの日恋に落ちた\n瞬間のドラマチック　フィルムの中のひとコマも\n消えないよ心に刻むから\n\n君だよ君なんだよ　教えてくれた
-暗闇も光るなら　星空になる\n悲しみを笑顔に　もう隠さないで\n煌めくどんな星も君を照らすから\n\n眠りも忘れて　迎えた朝日が　やたらと突き刺さる\n低気圧運ぶ頭痛だって　薄れる君に逢えば\n\n静寂はロマンチック　
-紅茶に溶けたシュガーのように
-全身に巡るよ君の声\n\n君だよ君なんだよ　笑顔をくれた
-涙も光るなら　流星になる
-傷ついたその手を　もう離さないで
-願いを込めた空に　明日が来るから\n\n導いてくれた光は君だよ
-つられて僕も走りだした\n\n知らぬ間にクロスし始めた
-ほら　今だ　そこで光るなら\n\n君だよ君なんだよ　教えてくれた
-暗闇は終わるから\n\n君だよ君なんだよ　教えてくれた
-暗闇も光るなら　星空になる
-悲しみを笑顔に　もう隠さないで
-煌めくどんな星も　君を照らすから\n\n答えはいつでも偶然？必然？
-いつか選んだ道こそ運命になる
-握りしめたその希望も不安も
-きっと二人を動かす光になるから",
+"poster":"https://i.pinimg.com/originals/1b/19/e8/1b19e81f39e005d18c0b48956bf76b92.gif",
+"title":"Siesta Chan",
+"title_orignal":"Test Page",
+"age":"18+",
+"works":"unknown",
+"country":"Kazakhstan, 2023 жыл",
+"i_video":"? бөлім. ~ 00 минут",
+"status":"?, ?",
+"studio":"Студия Symphony, автор Seele?",
+"available_player":"Қолжетімді бейне ойнатқыш: Web-Player",
+"note":"Бұл бетті әзірлеуші ​​тестілеу үшін қосқан!",
 "info":"Сипаттама берілген жоқ!",
-"announcement":"null",
+"premium":"null",
 "unavailable":"null",
-"player":"null",
+"announcement":"null",
 "comments":"null",
 "key":"test",
 "height":"null",
 "uncom":"null",
 "id":"test",
-"data":"",
-"description":"Тылсым күштер, тергеушілер"
+"data":"null",
+"description":"test, test, test, test"
 } ]
