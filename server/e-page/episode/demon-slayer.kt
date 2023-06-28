f4d4db0d164a91b2dcc442b9fb3d4053cdd22f59
dopamine-app <123301344+dopamine-app@users.noreply.github.com>
2023-06-28 22:33:06 +0600

    Update demon-slayer.kt

--- server/e-page/episode/demon-slayer.kt
+++ server/e-page/episode/demon-slayer.kt
@@ -2,9 +2,9 @@
 
 "episode":"1 бөлім",
 
-"is":"https://video.sibnet.ru/shell.php?videoid=5055502",
+"is":"https://dv98.sibnet.ru/47/24/12/4724121.mp4?st=Mlm2bO4x1cdFvI49x3heyA&e=1687999000&stor=34&noip=1",
 
-"url":"https://video.sibnet.ru/shell.php?videoid=5055502"
+"url":"https://dv98.sibnet.ru/47/24/12/4724121.mp4?st=Mlm2bO4x1cdFvI49x3heyA&e=1687999000&stor=34&noip=1"
 
 },
 
