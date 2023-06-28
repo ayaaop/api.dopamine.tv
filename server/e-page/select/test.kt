3899c1c78f95cee34124e7ad502b92f3653b4387
dopamine-app <123301344+dopamine-app@users.noreply.github.com>
2023-06-28 21:46:37 +0600

    Update test.kt

--- server/e-page/select/test.kt
+++ server/e-page/select/test.kt
@@ -1,1 +1,6 @@
-
+[ {
+  "studio_avatars":"https://www.animesenpai.net/wp-content/uploads/2022/09/maxresdefault-4-1024x576.jpg",
+  "studioName":"Symphony App Studio",
+  "episodeList":"1 бөлім",
+  "id":"test"
+} ]
