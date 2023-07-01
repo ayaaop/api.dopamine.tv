5d4f4e31b975da2f7812c2c29e85e922afbd1028
dopamine-app <123301344+dopamine-app@users.noreply.github.com>
2023-07-02 02:03:03 +0600

    Create black-panther.kt

---
+++ server/e-page/episode/black-panther.kt
@@ -0,0 +1,5 @@
+[ {
+  "episode":"Фильм",
+  "is":"https://vk.com/video_ext.php?oid=-70095498&id=456240541&hash=e1b0ab0c8f1ee279",
+  "url":"https://vk.com/video_ext.php?oid=-70095498&id=456240541&hash=e1b0ab0c8f1ee279"
+} ]
