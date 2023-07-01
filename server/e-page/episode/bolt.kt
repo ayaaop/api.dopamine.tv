9878a3ec1e334ec829d7164fb755fc308dba9eb1
dopamine-app <123301344+dopamine-app@users.noreply.github.com>
2023-07-02 02:19:21 +0600

    Create bolt.kt

---
+++ server/e-page/episode/bolt.kt
@@ -0,0 +1,5 @@
+[ {
+  "episode":"Фильм",
+  "is":"https://vk.com/video_ext.php?oid=-70095498&id=456240548&hash=d33c13de72b58cbb",
+  "url":"https://vk.com/video_ext.php?oid=-70095498&id=456240548&hash=d33c13de72b58cbb"
+} ]
