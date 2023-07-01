3f7eed550ef57a372d7b8f183269110d8ecba66c
dopamine-app <123301344+dopamine-app@users.noreply.github.com>
2023-07-02 02:04:13 +0600

    Update the-wolf-of-wall-street.kt

--- server/e-page/episode/the-wolf-of-wall-street.kt
+++ server/e-page/episode/the-wolf-of-wall-street.kt
@@ -1,5 +1,5 @@
 [ {
   "episode":"Фильм",
   "is":"https://vk.com/video_ext.php?oid=-217173314&id=456239099&hash=f99dd4fcc00989ce",
-  "url":"https://vk16-1.vkuser.net/?expires=1688223667451&srcIp=85.193.107.147&pr=40&srcAg=CHROME_ANDROID&ms=212.96.94.34&type=1&sig=klsUOcwO_-8&ct=0&urls=45.136.21.161;185.226.53.170&clientType=14&appId=512000384397&zs=33&id=3136426478178"
+  "url":"https://vk.com/video_ext.php?oid=-217173314&id=456239099&hash=f99dd4fcc00989ce"
 } ]
