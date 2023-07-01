9f601c25e825d5053b1c0e2f5c558d13e561242a
dopamine-app <123301344+dopamine-app@users.noreply.github.com>
2023-07-02 02:18:04 +0600

    Create bolt.kt

---
+++ server/e-page/select/bolt.kt
@@ -0,0 +1,6 @@
+[ {
+  "studio_avatars":"https://play-lh.googleusercontent.com/XITZmvMGzAoCeqk6ruVkhupKy3bdhuKYqwIIZyII5QJvHmS5TQ7Bfbtktjt99fEukPw",
+  "studioName":"QAZAQSTAN TV",
+  "episodeList":"Толықметражды",
+  "id":"bolt"
+} ]
