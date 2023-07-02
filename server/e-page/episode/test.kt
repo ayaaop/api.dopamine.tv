508e8ca08d93025e5211450add4971cd3cfe8e42
dopamine-app <123301344+dopamine-app@users.noreply.github.com>
2023-07-03 01:04:26 +0600

    Update test.kt

--- server/e-page/episode/test.kt
+++ server/e-page/episode/test.kt
@@ -6,4 +6,8 @@
  {"episode":"«Kodik WeyLand»",
   "is":"https://weyland.cloud.kodik-storage.com/useruploads/9bdd057c-c360-4b8b-9a5e-cdb60ef2ec00/cf6cb933fd04b15315b3962fe05dd825:2023070306/480.mp4:hls:manifest.m3u8",
   "url":"https://weyland.cloud.kodik-storage.com/useruploads/9bdd057c-c360-4b8b-9a5e-cdb60ef2ec00/cf6cb933fd04b15315b3962fe05dd825:2023070306/480.mp4:hls:manifest.m3u8"
+},
+ {"episode":"«Kodik Downloader»",
+  "is":"https://cloud.kodik-storage.com/useruploads/9bdd057c-c360-4b8b-9a5e-cdb60ef2ec00/0806551392e813fa00236c6b1e14840d:2023070300/360.mp4",
+  "url":"https://cloud.kodik-storage.com/useruploads/9bdd057c-c360-4b8b-9a5e-cdb60ef2ec00/0806551392e813fa00236c6b1e14840d:2023070300/360.mp4"
 } ]
