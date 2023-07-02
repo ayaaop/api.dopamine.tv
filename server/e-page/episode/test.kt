7cbbe2312a728f5763b0e4f5c9850217d1e75c0e
dopamine-app <123301344+dopamine-app@users.noreply.github.com>
2023-07-03 00:45:00 +0600

    Update test.kt

--- server/e-page/episode/test.kt
+++ server/e-page/episode/test.kt
@@ -1,9 +1,9 @@
 [ {
   "episode":"«Kodik UserUp»",
-  "is":"https://cloud.kodik-storage.com/useruploads/9bdd057c-c360-4b8b-9a5e-cdb60ef2ec00/cf6cb933fd04b15315b3962fe05dd825:2023070306/360.mp4:hls:manifest.m3u8",
-  "url":"https://cloud.kodik-storage.com/useruploads/9bdd057c-c360-4b8b-9a5e-cdb60ef2ec00/cf6cb933fd04b15315b3962fe05dd825:2023070306/360.mp4:hls:manifest.m3u8"
+  "is":"https://cloud.kodik-storage.com/useruploads/9bdd057c-c360-4b8b-9a5e-cdb60ef2ec00/cf6cb933fd04b15315b3962fe05dd825:2023070306/480.mp4:hls:manifest.m3u8",
+  "url":"https://cloud.kodik-storage.com/useruploads/9bdd057c-c360-4b8b-9a5e-cdb60ef2ec00/cf6cb933fd04b15315b3962fe05dd825:2023070306/480.mp4:hls:manifest.m3u8"
 },
  {"episode":"«Kodik WeyLand»",
-  "is":"https://weyland.cloud.kodik-storage.com/useruploads/9bdd057c-c360-4b8b-9a5e-cdb60ef2ec00/cf6cb933fd04b15315b3962fe05dd825:2023070306/360.mp4:hls:manifest.m3u8",
-  "url":"https://weyland.cloud.kodik-storage.com/useruploads/9bdd057c-c360-4b8b-9a5e-cdb60ef2ec00/cf6cb933fd04b15315b3962fe05dd825:2023070306/360.mp4:hls:manifest.m3u8"
+  "is":"https://weyland.cloud.kodik-storage.com/useruploads/9bdd057c-c360-4b8b-9a5e-cdb60ef2ec00/cf6cb933fd04b15315b3962fe05dd825:2023070306/480.mp4:hls:manifest.m3u8",
+  "url":"https://weyland.cloud.kodik-storage.com/useruploads/9bdd057c-c360-4b8b-9a5e-cdb60ef2ec00/cf6cb933fd04b15315b3962fe05dd825:2023070306/480.mp4:hls:manifest.m3u8"
 } ]
