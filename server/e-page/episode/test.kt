737310843d21eaf6a36be30e58f3b1989d9d9864
dopamine-app <123301344+dopamine-app@users.noreply.github.com>
2023-07-03 00:26:19 +0600

    Update test.kt

--- server/e-page/episode/test.kt
+++ server/e-page/episode/test.kt
@@ -1,5 +1,9 @@
 [ {
-  "episode":"1 бөлім",
-  "is":"https://v19-web-newkey.tiktokcdn.com/f420644810c1a2ba3f41626c1db36a1d/649cb39a/video/tos/useast2a/tos-useast2a-pve-0037-aiso/oUHfeQEonbT67EvlgDbrA2gzveXaHHGEibGrJf/?a=1988&ch=0&cr=0&dr=0&lr=tiktok&cd=0|0|1|0&cv=1&br=3682&bt=1841&cs=0&ds=3&ft=ApKJEBQrq8ZmoV_Otc_vjtHQ8AhLrus&mime_type=video_mp4&qs=0&rc=ZDhoaWc7PDY7aDgzaWU6OUBpamxxdWc6ZjR1aTMzZjgzM0BhY2AyYGM0NS0xNi41NGA0YSNlYzZpcjQwcXNgLS1kL2Nzcw==&l=2023062816261683ED95E72E14C316C0C1&btag=e00080000",
-  "url":"https://v19-web-newkey.tiktokcdn.com/f420644810c1a2ba3f41626c1db36a1d/649cb39a/video/tos/useast2a/tos-useast2a-pve-0037-aiso/oUHfeQEonbT67EvlgDbrA2gzveXaHHGEibGrJf/?a=1988&ch=0&cr=0&dr=0&lr=tiktok&cd=0|0|1|0&cv=1&br=3682&bt=1841&cs=0&ds=3&ft=ApKJEBQrq8ZmoV_Otc_vjtHQ8AhLrus&mime_type=video_mp4&qs=0&rc=ZDhoaWc7PDY7aDgzaWU6OUBpamxxdWc6ZjR1aTMzZjgzM0BhY2AyYGM0NS0xNi41NGA0YSNlYzZpcjQwcXNgLS1kL2Nzcw==&l=2023062816261683ED95E72E14C316C0C1&btag=e00080000"
+  "episode":"«Kodik UserUp»",
+  "is":"https://cloud.kodik-storage.com/useruploads/9bdd057c-c360-4b8b-9a5e-cdb60ef2ec00/cf6cb933fd04b15315b3962fe05dd825:2023070306/360.mp4:hls:manifest.m3u8",
+  "url":"https://cloud.kodik-storage.com/useruploads/9bdd057c-c360-4b8b-9a5e-cdb60ef2ec00/cf6cb933fd04b15315b3962fe05dd825:2023070306/360.mp4:hls:manifest.m3u8"
+},
+ {"episode":"«Kodik WeyLand»",
+  "is":"https://weyland.cloud.kodik-storage.com/useruploads/9bdd057c-c360-4b8b-9a5e-cdb60ef2ec00/cf6cb933fd04b15315b3962fe05dd825:2023070306/360.mp4:hls:manifest.m3u8",
+  "url":"https://weyland.cloud.kodik-storage.com/useruploads/9bdd057c-c360-4b8b-9a5e-cdb60ef2ec00/cf6cb933fd04b15315b3962fe05dd825:2023070306/360.mp4:hls:manifest.m3u8"
 } ]
