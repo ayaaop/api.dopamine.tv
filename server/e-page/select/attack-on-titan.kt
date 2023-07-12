f700e2c3f9e600fc7e6a3ccd60761f9572a54c3a
dopamine-app <123301344+dopamine-app@users.noreply.github.com>
2023-07-13 00:18:48 +0600

    Update attack-on-titan.kt

--- server/e-page/select/attack-on-titan.kt
+++ server/e-page/select/attack-on-titan.kt
@@ -1,6 +1,6 @@
 [ {
   "studio_avatars":"https://dopamine-app.github.io/api.dopamine.tv/server/studio-logo/dopamine.voice-logo.png",
   "studioName":"Dopamine Voice",
-  "episodeList":"12 бөлім",
+  "episodeList":"1 бөлім",
   "id":"attack-on-titan"
 } ]
