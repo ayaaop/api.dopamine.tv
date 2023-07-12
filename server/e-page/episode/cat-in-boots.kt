34f5d2b4716e2a475b4e55b4ffcc147cafbd152b
dopamine-app <123301344+dopamine-app@users.noreply.github.com>
2023-07-12 23:52:58 +0600

    Update cat-in-boots.kt

--- server/e-page/episode/cat-in-boots.kt
+++ server/e-page/episode/cat-in-boots.kt
@@ -1,5 +1,5 @@
 [ {
 "episode":"Фильм",
 "is":"https://vk.com/video_ext.php?oid=-217173314&id=456239459&hash=450373e1e5cd5e7b",
-"url":"http://anifon.tk:1212/AgADFC769"
+"url":"https://firebasestorage.googleapis.com/v0/b/dopamine-voice.appspot.com/o/Puss%20in%20Boots%3A%20The%20Last%20Wish%20(2022)%2F%D0%95%D1%82%D1%96%D0%BA%20%D0%9A%D0%B8%D0%B3%D0%B5%D0%BD%20%D0%9C%D1%8B%D1%81%D1%8B%D2%9B.mp4?alt=media&token=56b05016-6aaf-4727-a0bd-d1d65ab23848"
 } ]
