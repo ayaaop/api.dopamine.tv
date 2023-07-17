cbc15b9145468ea6922548a8e8b8e117094b6430
dopamine-app <123301344+dopamine-app@users.noreply.github.com>
2023-07-17 11:06:50 +0600

    Update yakhan-yeongung.kt

--- server/e-page/select/yakhan-yeongung.kt
+++ server/e-page/select/yakhan-yeongung.kt
@@ -1,6 +1,6 @@
 [ {
   "studio_avatars":"https://sun9-77.userapi.com/impg/hYtvdibPINhi5Gt1Oe3e6muvBeEUzKP-oyJ3fQ/NQExV_2cdFs.jpg?size=387x604&quality=95&sign=0ca7a4b04f7d64dbab25d40169346963&type=album",
   "studioName":"New Dem",
-  "episodeList":"1 бөлім",
+  "episodeList":"2 бөлім",
   "id":"yakhan-yeongung"
 } ]
