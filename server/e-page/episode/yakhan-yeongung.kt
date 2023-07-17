27990f42ee4a1536c475959a3386ae0965a62254
dopamine-app <123301344+dopamine-app@users.noreply.github.com>
2023-07-17 11:06:34 +0600

    Update yakhan-yeongung.kt

--- server/e-page/episode/yakhan-yeongung.kt
+++ server/e-page/episode/yakhan-yeongung.kt
@@ -2,4 +2,8 @@
   "episode":"1 бөлім",
   "is":"https://vk.com/video_ext.php?oid=-221394558&id=456239025&hash=889e67435fbe9f5e",
   "url":"https://vk.com/video_ext.php?oid=-221394558&id=456239025&hash=889e67435fbe9f5e"
-} ]
+},
+ {"episode":"2 бөлім",
+   "is":"https://vk.com/video_ext.php?oid=-221394558&id=456239027&hash=443aed157f854bc7",
+   "url":"https://vk.com/video_ext.php?oid=-221394558&id=456239027&hash=443aed157f854bc7"
+ } ]
