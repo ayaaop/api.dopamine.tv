2f255d2e07774c6e513e0e47d58e941b8fbde551
dopamine-app <123301344+dopamine-app@users.noreply.github.com>
2023-07-30 13:27:12 +0600

    Update vanitas-no-carte.kt

--- server/e-page/episode/vanitas-no-carte.kt
+++ server/e-page/episode/vanitas-no-carte.kt
@@ -2,4 +2,8 @@
 "episode":"1 бөлім",
 "is":"https://video.sibnet.ru/shell.php?videoid=5174497",
 "url":"https://video.sibnet.ru/shell.php?videoid=5174497"
+},
+ {"episode":"2 бөлім",
+   "is":"https://video.sibnet.ru/shell.php?videoid=5207975",
+   "url":"https://video.sibnet.ru/shell.php?videoid=5207975"
 } ]
