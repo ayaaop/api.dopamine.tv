8eecfc5bc4ec2bf9a718f247a3379f2719f5cb98
dopamine-app <123301344+dopamine-app@users.noreply.github.com>
2023-07-30 13:27:25 +0600

    Update vanitas-no-carte.kt

--- server/e-page/select/vanitas-no-carte.kt
+++ server/e-page/select/vanitas-no-carte.kt
@@ -1,6 +1,6 @@
 [ {
   "studio_avatars":"https://dopamine-app.github.io/api.dopamine.tv/server/studio-logo/special.production-logo.png",
   "studioName":"Special Production",
-  "episodeList":"1 бөлім",
+  "episodeList":"2 бөлім",
   "id":"vanitas-no-carte"
 } ]
